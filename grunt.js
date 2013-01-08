760bb5e59ba2387003ca73bbc7def4dfc58c7a58
Rich Harris <richard.a.harris@gmail.com>
2013-01-08 18:53:02 +0000

    replaced spaces with tabs, like a sane person

--- grunt.js
+++ grunt.js
@@ -1,73 +1,73 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n' +
-        '\n' +
-        '/*jslint eqeq: true, plusplus: true */\n' +
-        '/*global document, HTMLElement */\n' +
-        '\n' +
-        '\'use strict\';\n\n',
-      amd_start: 'define([], function() { ',
-      amd_end: ' return Anglebars; \n});'
-    },
-    lint: {
-      files: ['grunt.js', 'src/**/*.js'] // TODO add tests
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    concat: {
-      dist: {
-        src: ['<banner:meta.banner>', 'src/Anglebars.js', 'src/utils.js', 'src/ViewModel.js', 'src/DOMViews.js', 'src/TextViews.js'],
-        dest: 'build/<%= pkg.name %>.js'
-      },
-      amd : {
-        src: ['<banner:meta.amd_start>', 'build/<%= pkg.name %>.js', '<banner:meta.amd_end>'],
-        dest:'build/<%= pkg.name %>.amd.js'
-      }
-    },
-    min: {
-      dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-    watch: {
-      files: ['<config:lint.files>', 'package.json'],
-      tasks: ['concat','min']
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        nomen: false,
-        white: false
-      },
-      globals: {
-        console: true
-      }
-    },
-    uglify: {}
-  });
+	// Project configuration.
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+		meta: {
+			banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+				'<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+				'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+				' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n' +
+				'\n' +
+				'/*jslint eqeq: true, plusplus: true */\n' +
+				'/*global document, HTMLElement */\n' +
+				'\n' +
+				'\'use strict\';\n\n',
+			amd_start: 'define([], function() { ',
+			amd_end: ' return Anglebars; \n});'
+		},
+		lint: {
+			files: ['grunt.js', 'src/**/*.js'] // TODO add tests
+		},
+		qunit: {
+			files: ['test/**/*.html']
+		},
+		concat: {
+			dist: {
+				src: ['<banner:meta.banner>', 'src/Anglebars.js', 'src/utils.js', 'src/ViewModel.js', 'src/DOMViews.js', 'src/TextViews.js'],
+				dest: 'build/<%= pkg.name %>.js'
+			},
+			amd : {
+				src: ['<banner:meta.amd_start>', 'build/<%= pkg.name %>.js', '<banner:meta.amd_end>'],
+				dest:'build/<%= pkg.name %>.amd.js'
+			}
+		},
+		min: {
+			dist: {
+				src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+				dest: 'build/<%= pkg.name %>.min.js'
+			}
+		},
+		watch: {
+			files: ['<config:lint.files>', 'package.json'],
+			tasks: ['concat','min']
+		},
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				boss: true,
+				eqnull: true,
+				browser: true,
+				nomen: false,
+				white: false
+			},
+			globals: {
+				console: true
+			}
+		},
+		uglify: {}
+	});
 
-  // Default task.
-  grunt.registerTask('default', 'lint qunit concat min');
+	// Default task.
+	grunt.registerTask('default', 'lint qunit concat min');
 
 };
