9db88b9c8f5bf93e6829ae7e9a15c82f2328f6ad
Rich Harris <richard.a.harris@gmail.com>
2013-03-17 12:39:36 +0000

    support passing in jQuery collections etc. also CSS selectors

--- src/Anglebars.js
+++ src/Anglebars.js
@@ -248,11 +248,23 @@ getEl = function ( input ) {
 
 	// Get node from string
 	if ( typeof input === 'string' ) {
+		// try ID first
 		output = document.getElementById( input );
 
+		// then as selector, if possible
+		if ( !output && document.querySelector ) {
+			output = document.querySelector( input );
+		}
+
+		// did it work?
 		if ( output.tagName ) {
 			return output;
 		}
+	}
+
+	// If we've been given a collection (jQuery, Zepto etc), extract the first item
+	if ( input[0] && input[0].tagName ) {
+		return input[0];
 	}
 
 	throw new Error( 'Could not find container element' );
