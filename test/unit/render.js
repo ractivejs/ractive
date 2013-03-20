1cc60d8e3cc30763c4715902559f707414ce5aca
Rich Harris <richard.a.harris@gmail.com>
2013-03-20 09:30:23 +0000

    added function value test, moved test samples into .js file to allow for this

--- test/unit/render.js
+++ test/unit/render.js
@@ -9,27 +9,95 @@
 
 QUnit.config.reorder = false;
 
-var fixture = document.getElementById( 'qunit-fixture' );
+var fixture = document.getElementById( 'qunit-fixture' ), tests;
 
-test( 'Anglebars prototype has a render method', function () {
-	ok( _.isFunction( Anglebars.prototype.render ) );
-});
+tests = [
+	{
+		name: "Empty model",
+		template: "",
+		result: ""
+	},
+	{
+		name: "Plain text",
+		template: "a string",
+		result: "a string"
+	},
+	{
+		name: "Single interpolator",
+		template: "{{mustache}}",
+		data: { mustache: "hello world" },
+		result: "hello world"
+	},
+	{
+		name: "Section with index refs",
+		template: "<ul>{{#items:i}}<li>{{i}}: {{name}}</li>{{/items}}</ul>",
+		data: {
+			items: [{name:"zero"},{name:"one"},{name:"two"}]
+		},
+		result: "<ul><li>0: zero</li><li>1: one</li><li>2: two</li></ul>"
+	},
+	{
+		name: "Section with index refs in attributes",
+		template: "<ul>{{#items:i}}<li data-id=\"{{i}}\">{{name}}</li>{{/items}}</ul>",
+		data: {
+			items: [{name:"zero"},{name:"one"},{name:"two"}]
+		},
+		result: "<ul><li data-id=\"0\">zero</li><li data-id=\"1\">one</li><li data-id=\"2\">two</li></ul>"
+	},
+	{
+		name: "Element with attributes",
+		template: "<p class='{{class_name}}'>text</p>",
+		data: {
+			"class_name": "it_works"
+		},
+		result: "<p class=\"it_works\">text</p>"
+	},
+	{
+		name: "Section with descendant attributes",
+		template: "{{#todos}}<li><label>{{todo}}</label><input value='{{todo}}'></li>{{/todos}}",
+		data: {
+			todos: [{todo:"debug Anglebars"},{todo:"release Anglebars"}]
+		},
+		result: "<li><label>debug Anglebars</label><input value=\"debug Anglebars\"></li><li><label>release Anglebars</label><input value=\"release Anglebars\"></li>"
+	},
+	{
+		name: "Partials",
+		template: "{{#items}}{{>item}}{{/items}}",
+		data: {
+			items: [{description:"Item the first"},{description:"Item the second"},{description:"Item the third"}]
+		},
+		partials: {
+			item: "<p>{{description}}</p>"
+		},
+		result: "<p>Item the first</p><p>Item the second</p><p>Item the third</p>"
+	},
+	{
+		name: "Empty string attributes",
+		template: "<p class=\"\">test</p>",
+		result: "<p class=\"\">test</p>"
+	},
+	{
+		name: 'Function values',
+		template: '<p>{{message}}</p>',
+		data: { message: function () { return 'functions work'; } },
+		result: '<p>functions work</p>'
+	}
+];
 
-$.getJSON( 'samples/render.json' ).done( function ( data ) {
-	_.each( data, function ( t, i ) {
-		test( t.name, function () {
-			console.group(i+2);
 
-			var anglebars = new Anglebars({
-				el: fixture,
-				data: t.data,
-				template: t.template,
-				partials: t.partials
-			});
+_.each( tests, function ( t, i ) {
+	test( t.name, function () {
+		console.group(i+2);
 
-			equal( fixture.innerHTML, t.result );
-
-			console.groupEnd();
+		var anglebars = new Anglebars({
+			el: fixture,
+			data: t.data,
+			template: t.template,
+			partials: t.partials
 		});
+
+		equal( fixture.innerHTML, t.result );
+
+		console.groupEnd();
 	});
 });
