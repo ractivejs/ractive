3f4ce4217d306b7835d4525e5c28c6bef74ff9de
Rich Harris <richard.a.harris@gmail.com>
2013-03-24 16:55:30 +0000

    added addEventListener polyfill

--- src/legacy.js
+++ src/legacy.js
@@ -38,6 +38,62 @@
 		};
 	}
 
+	// https://gist.github.com/2864711/946225eb3822c203e8d6218095d888aac5e1748e
+	// addEventListener polyfill 1.0 / Eirik Backer / MIT Licence
+	(function( win, doc ){
+		var docHijack, addEvent, addListen;
+
+		if ( win.addEventListener ) {
+			return;
+		}
+	 
+		docHijack = function ( p ) {
+			var old = doc[p];
+			doc[p] = function(v){
+				return addListen( old( v ) );
+			};
+		};
+
+		addEvent = function ( on, fn, self ) {
+			return ( self = this ).attachEvent( 'on' + on, function ( e ) {
+				e = e || win.event;
+				e.preventDefault  = e.preventDefault  || function () { e.returnValue = false; };
+				e.stopPropagation = e.stopPropagation || function () { e.cancelBubble = true; };
+				fn.call( self, e );
+			});
+		};
+
+		addListen = function ( obj, i ) {
+			if ( i = obj.length ) {
+				while ( i-- ) {
+					obj[i].addEventListener = addEvent;
+				}
+			}
+
+			else {
+				obj.addEventListener = addEvent;
+			}
+
+			return obj;
+		};
+	 
+		addListen([ doc, win ]);
+
+		if ( 'Element' in win ) { //IE8
+			win.Element.prototype.addEventListener = addEvent;
+		}
+
+		else { //IE < 8
+			doc.attachEvent( 'onreadystatechange', function () {
+				addListen( doc.all );
+			}); //Make sure we also init at domReady
+			docHijack( 'getElementsByTagName' );
+			docHijack( 'getElementById' );
+			docHijack( 'createElement' );
+			addListen( doc.all ); 
+		}
+	})( window, document );
+
 
 	// Array extras
 	if ( !Array.prototype.indexOf ) {
