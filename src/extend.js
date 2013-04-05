c049e38167b7fc77321544f5abe8a97d4b257734
Rich Harris <richard.a.harris@gmail.com>
2013-04-05 15:51:41 +0100

    typo

--- src/extend.js
+++ src/extend.js
@@ -26,7 +26,7 @@
 		// extend child with specified methods, as long as they don't override Ractive.prototype methods
 		for ( key in childProps ) {
 			if ( childProps.hasOwnProperty( key ) ) {
-				if ( A.prototype.hasOwnProperty( key ) ) {
+				if ( R.prototype.hasOwnProperty( key ) ) {
 					throw new Error( 'Cannot override "' + key + '" method or property of Ractive prototype' );
 				}
 
