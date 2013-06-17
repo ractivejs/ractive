412c79476c5cea8aa7706f9d6414a4ede4152719
Rich Harris <richard.a.harris@gmail.com>
2013-06-17 17:21:10 +0100

    add defineProperties and createFromNull helpers to begin.js

--- wrapper/begin.js
+++ wrapper/begin.js
@@ -25,6 +25,9 @@ isObject,
 isNumeric,
 isEqual,
 getEl,
+defineProperty,
+defineProperties,
+createFromNull,
 noop = function () {},
 
 
@@ -116,3 +119,48 @@ namespaces = {
 	xml:    'http://www.w3.org/XML/1998/namespace',
 	xmlns:  'http://www.w3.org/2000/xmlns/'
 };
+
+
+
+// we're creating a defineProperty function here - we don't want to add
+// this to _legacy.js since it's not a polyfill. It won't allow us to set
+// non-enumerable properties. That shouldn't be a problem, unless you're
+// using for...in on a (modified) array, in which case you deserve what's
+// coming anyway
+try {
+	Object.defineProperty({}, 'test', { value: 0 });
+	Object.defineProperties({}, { test: { value: 0 } });
+
+	defineProperty = Object.defineProperty;
+	defineProperties = Object.defineProperties;
+} catch ( err ) {
+	// Object.defineProperty doesn't exist, or we're in IE8 where you can
+	// only use it with DOM objects (what the fuck were you smoking, MSFT?)
+	defineProperty = function ( obj, prop, desc ) {
+		obj[ prop ] = desc.value;
+	};
+
+	defineProperties = function ( obj, props ) {
+		var prop;
+
+		for ( prop in props ) {
+			if ( props.hasOwnProperty( prop ) ) {
+				defineProperty( obj, prop, props[ prop ] );
+			}
+		}
+	};
+}
+
+
+try {
+	Object.create( null );
+
+	createFromNull = function () {
+		return Object.create( null );
+	};
+} catch ( err ) {
+	// sigh
+	createFromNull = function () {
+		return {}; // hope you're not modifying the Object prototype
+	}
+}
