c44ce9530d8509437a6f9549ce059e5799fdb298
Rich Harris <richard.a.harris@gmail.com>
2013-06-19 23:49:40 +0100

    fixed problem with subclasses of subclasses not inheriting certain properties from their parent

--- src/Ractive/Ractive.extend.js
+++ src/Ractive/Ractive.extend.js
@@ -1,122 +1,224 @@
-var fillGaps = function ( target, source ) {
-	var key;
+(function () {
 
-	for ( key in source ) {
-		if ( source.hasOwnProperty( key ) && !target.hasOwnProperty( key ) ) {
-			target[ key ] = source[ key ];
+	var fillGaps,
+		clone,
+		augment,
+
+		inheritFromParent,
+		inheritFromChildProps,
+		conditionallyParseTemplate,
+		extractInlinePartials,
+		conditionallyParsePartials,
+		initChildInstance,
+
+		extendable,
+		inheritable,
+		blacklist;
+
+	extend = function ( childProps ) {
+
+		var Parent, Child, key, template, partials, partial;
+
+		Parent = this;
+
+		// create Child constructor
+		Child = function ( options ) {
+			initChildInstance( this, Child, options );
+		};
+
+		// inherit options from parent, if we're extending a subclass
+		if ( Parent !== Ractive ) {
+			inheritFromParent( Child, Parent );
 		}
-	}
-};
 
-extend = function ( childProps ) {
+		// extend child with parent methods
+		for ( key in Parent.prototype ) {
+			if ( Parent.prototype.hasOwnProperty( key ) ) {
+				Child.prototype[ key ] = Parent.prototype[ key ];
+			}
+		}
 
-	var Parent, Child, key, inheritedOptions, blacklist, template, partials, partial;
+		// apply childProps
+		inheritFromChildProps( Child, childProps );
 
-	Parent = this;
+		// parse template and any partials that need it
+		conditionallyParseTemplate( Child );
+		extractInlinePartials( Child );
+		conditionallyParsePartials( Child );
 
-	inheritedOptions = [ 'el', 'preserveWhitespace', 'append', 'twoway', 'modifyArrays' ];
-	blacklist = inheritedOptions.concat( 'data', 'template', 'partials', 'transitions' );
+		Child.extend = Parent.extend;
 
-	// Parse template
-	if ( childProps.template ) {
-		if ( typeof childProps.template === 'string' ) {
+		return Child;
+	};
+
+	extendable = [ 'data', 'partials', 'transitions' ];
+	inheritable = [ 'el', 'template', 'complete', 'modifyArrays', 'twoway', 'lazy', 'append', 'preserveWhitespace', 'sanitize' ];
+	blacklist = extendable.concat( inheritable );
+
+	inheritFromParent = function ( Child, Parent ) {
+		extendable.forEach( function ( property ) {
+			if ( Parent[ property ] ) {
+				Child[ property ] = clone( Parent[ property ] );
+			}
+		});
+
+		inheritable.forEach( function ( property ) {
+			if ( Parent[ property ] !== undefined ) {
+				Child[ property ] = Parent[ property ];
+			}
+		});
+	};
+
+	inheritFromChildProps = function ( Child, childProps ) {
+		var key;
+
+		extendable.forEach( function ( property ) {
+			var value = childProps[ property ];
+
+			if ( value ) {
+				if ( Child[ property ] ) {
+					augment( Child[ property ], value );
+				}
+
+				else {
+					Child[ property ] = value;
+				}
+			}
+		});
+
+		inheritable.forEach( function ( property ) {
+			if ( childProps[ property ] !== undefined ) {
+				Child[ property ] = childProps[ property ];
+			}
+		});
+
+		// Extend child with specified methods, as long as they don't override Ractive.prototype methods.
+		// Blacklisted properties don't extend the child, as they are part of the initialisation options
+		for ( key in childProps ) {
+			if ( childProps.hasOwnProperty( key ) && blacklist.indexOf( key ) === -1 ) {
+				if ( Ractive.prototype.hasOwnProperty( key ) ) {
+					throw new Error( 'Cannot override "' + key + '" method or property of Ractive prototype' );
+				}
+
+				Child.prototype[ key ] = childProps[ key ];
+			}
+		}
+	};
+
+	conditionallyParseTemplate = function ( Child ) {
+		if ( typeof Child.template === 'string' ) {
 			if ( !Ractive.parse ) {
 				throw new Error( missingParser );
 			}
 
-			template = Ractive.parse( childProps.template );
-		} else {
-			template = childProps.template;
-		}
-	}
-
-	// Parse partials, if necessary
-	if ( childProps.partials ) {
-		partials = {};
-
-		for ( key in childProps.partials ) {
-			if ( childProps.partials.hasOwnProperty( key ) ) {
-				if ( typeof childProps.partials[ key ] === 'string' ) {
-					if ( !Ractive.parse ) {
-						throw new Error( missingParser );
-					}
-
-					partial = Ractive.parse( childProps.partials[ key ], childProps );
-				} else {
-					partial = childProps.partials[ key ];
-				}
-
-				partials[ key ] = partial;
-			}
-		}
-	}
-
-	Child = function ( options ) {
-		var key, i, optionName;
-
-		// Add template to options, if necessary
-		if ( !options.template && template ) {
-			options.template = template;
-		}
-
-		// Extend subclass data with instance data
-		if ( !options.data ) {
-			options.data = {};
-		}
-
-		fillGaps( options.data, childProps.data );
-
-		// Transitions
-		if ( !options.transitions ) {
-			options.transitions = {};
-		}
-
-		fillGaps( options.transitions, childProps.transitions );
-
-		// Add in preparsed partials
-		if ( partials ) {
-			if ( !options.partials ) {
-				options.partials = {};
-			}
-
-			fillGaps( options.partials, partials );
-		}
-
-		i = inheritedOptions.length;
-		while ( i-- ) {
-			optionName = inheritedOptions[i];
-			if ( !options.hasOwnProperty( optionName ) && childProps.hasOwnProperty( optionName ) ) {
-				options[ optionName ] = childProps[ optionName ];
-			}
-		}
-
-		Ractive.call( this, options );
-
-		if ( this.init ) {
-			this.init.call( this, options );
+			Child.template = Ractive.parse( Child.template, Child ); // all the relevant options are on Child
 		}
 	};
 
-	// extend child with parent methods
-	for ( key in Parent.prototype ) {
-		if ( Parent.prototype.hasOwnProperty( key ) ) {
-			Child.prototype[ key ] = Parent.prototype[ key ];
-		}
-	}
-
-	// Extend child with specified methods, as long as they don't override Ractive.prototype methods.
-	// Blacklisted properties don't extend the child, as they are part of the initialisation options
-	for ( key in childProps ) {
-		if ( childProps.hasOwnProperty( key ) && blacklist.indexOf( key ) === -1 ) {
-			if ( Ractive.prototype.hasOwnProperty( key ) ) {
-				throw new Error( 'Cannot override "' + key + '" method or property of Ractive prototype' );
+	extractInlinePartials = function ( Child ) {
+		// does our template contain inline partials?
+		if ( isObject( Child.template ) ) {
+			if ( !Child.partials ) {
+				Child.partials = {};
 			}
 
-			Child.prototype[ key ] = childProps[ key ];
+			// get those inline partials
+			augment( Child.partials, Child.template.partials );
+
+			// but we also need to ensure that any explicit partials override inline ones
+			if ( childProps.partials ) {
+				augment( Child.partials, childProps.partials );
+			}
+
+			// move template to where it belongs
+			Child.template = Child.template.template;
 		}
-	}
+	};
 
-	Child.extend = Parent.extend;
+	conditionallyParsePartials = function ( Child ) {
+		var key, partial;
 
-	return Child;
-};
+		// Parse partials, if necessary
+		if ( Child.partials ) {
+			for ( key in Child.partials ) {
+				if ( Child.partials.hasOwnProperty( key ) ) {
+					if ( typeof Child.partials[ key ] === 'string' ) {
+						if ( !Ractive.parse ) {
+							throw new Error( missingParser );
+						}
+
+						partial = Ractive.parse( Child.partials[ key ], Child );
+					} else {
+						partial = Child.partials[ key ];
+					}
+
+					Child.partials[ key ] = partial;
+				}
+			}
+		}
+	};
+
+	initChildInstance = function ( child, Child, options ) {
+		var key, i, optionName;
+
+		// Add template to options, if necessary
+		if ( !options.template && Child.template ) {
+			options.template = Child.template;
+		}
+
+		extendable.forEach( function ( property ) {
+			if ( !options[ property ] ) {
+				if ( Child[ property ] ) {
+					options[ property ] = clone( Child[ property ] );
+				}
+			} else {
+				fillGaps( options[ property ], Child[ property ] );
+			}
+		});
+		
+		inheritable.forEach( function ( property ) {
+			if ( options[ property ] === undefined && Child[ property ] !== undefined ) {
+				options[ property ] = Child[ property ];
+			}
+		});
+
+		Ractive.call( child, options );
+
+		if ( child.init ) {
+			child.init.call( child, options );
+		}
+	};
+
+	fillGaps = function ( target, source ) {
+		var key;
+
+		for ( key in source ) {
+			if ( source.hasOwnProperty( key ) && !target.hasOwnProperty( key ) ) {
+				target[ key ] = source[ key ];
+			}
+		}
+	};
+
+	clone = function ( source ) {
+		var target = {}, key;
+
+		for ( key in source ) {
+			if ( source.hasOwnProperty( key ) ) {
+				target[ key ] = source[ key ];
+			}
+		}
+
+		return target;
+	};
+
+	augment = function ( target, source ) {
+		var key;
+
+		for ( key in source ) {
+			if ( source.hasOwnProperty( key ) ) {
+				target[ key ] = source[ key ];
+			}
+		}
+	};
+
+}());
