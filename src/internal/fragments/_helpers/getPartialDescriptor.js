a0d4d2e07a7a4457bae43b46114dac8ef56b7eac
Rich Harris <richard.a.harris@gmail.com>
2013-06-20 18:33:32 +0100

    refactored getPartialDescriptor

--- src/internal/fragments/_helpers/getPartialDescriptor.js
+++ src/internal/fragments/_helpers/getPartialDescriptor.js
@@ -1,27 +1,23 @@
-var getPartialDescriptor = function ( root, name ) {
-	var el, partial;
+var getPartialDescriptor;
 
-	// If the partial was specified on this instance, great
-	if ( root.partials && root.partials[ name ] ) {
-		return root.partials[ name ];
-	}
+(function () {
 
-	// If not, is it a global partial?
-	if ( Ractive.partials[ name ] ) {
-		
-		// If this was added manually to the registry, but hasn't been parsed,
-		// parse it now
-		if ( typeof Ractive.partials[ name ] === 'string' ) {
-			if ( !Ractive.parse ) {
-				throw new Error( missingParser );
-			}
+	var getPartialFromRegistry, unpack;
 
-			Ractive.partials[ name ] = Ractive.parse( Ractive.partials[ name ] );
+	getPartialDescriptor = function ( root, name ) {
+		var el, partial;
+
+		// If the partial was specified on this instance, great
+		if ( partial = getPartialFromRegistry( root, name ) ) {
+			return partial;
 		}
-	}
 
-	// Does it exist on the page as a script tag?
-	else {
+		// If not, is it a global partial?
+		if ( partial = getPartialFromRegistry( Ractive, name ) ) {
+			return partial;
+		}
+
+		// Does it exist on the page as a script tag?
 		el = doc.getElementById( name );
 		if ( el && el.tagName === 'SCRIPT' ) {
 			if ( !Ractive.parse ) {
@@ -30,23 +26,45 @@ var getPartialDescriptor = function ( root, name ) {
 
 			Ractive.partials[ name ] = Ractive.parse( el.innerHTML );
 		}
-	}
 
-	partial = Ractive.partials[ name ];
+		partial = Ractive.partials[ name ];
 
-	// No match? Return an empty array
-	if ( !partial ) {
-		if ( root.debug && console && console.warn ) {
-			console.warn( 'Could not find descriptor for partial "' + name + '"' );
+		// No match? Return an empty array
+		if ( !partial ) {
+			if ( root.debug && console && console.warn ) {
+				console.warn( 'Could not find descriptor for partial "' + name + '"' );
+			}
+
+			return [];
 		}
 
-		return [];
-	}
+		return unpack( partial );
+	};
 
-	// Unpack string, if necessary
-	if ( partial.length === 1 && typeof partial[0] === 'string' ) {
-		return partial[0];
-	}
+	getPartialFromRegistry = function ( registry, name ) {
+		if ( registry.partials[ name ] ) {
+			
+			// If this was added manually to the registry, but hasn't been parsed,
+			// parse it now
+			if ( typeof registry.partials[ name ] === 'string' ) {
+				if ( !Ractive.parse ) {
+					throw new Error( missingParser );
+				}
 
-	return partial;
-};
+				registry.partials[ name ] = Ractive.parse( registry.partials[ name ] );
+			}
+
+			return unpack( registry.partials[ name ] );
+		}
+	};
+
+	unpack = function ( partial ) {
+		// Unpack string, if necessary
+		if ( partial.length === 1 && typeof partial[0] === 'string' ) {
+			return partial[0];
+		}
+
+		return partial;
+	};
+
+}());
