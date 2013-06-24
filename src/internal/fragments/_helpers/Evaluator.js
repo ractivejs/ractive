9738e79350aab55fdc2c5dcdacceea4ed8cfa42f
Rich Harris <richard.a.harris@gmail.com>
2013-06-24 18:55:35 +0100

    update non-self-updating evaluators during refresh

--- src/internal/fragments/_helpers/Evaluator.js
+++ src/internal/fragments/_helpers/Evaluator.js
@@ -84,9 +84,18 @@
 		// This method forces the evaluator to sync with the current model
 		// in the case of a smart update
 		refresh: function () {
+			if ( !this.selfUpdating ) {
+				this.deferred = true;
+			}
+
 			var i = this.refs.length;
 			while ( i-- ) {
 				this.refs[i].update();
+			}
+
+			if ( this.deferred ) {
+				this.update();
+				this.deferred = false;
 			}
 		}
 	};
