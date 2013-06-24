ef327222fda894db5c32d5b8d1992ea2805518bc
Rich Harris <richard.a.harris@gmail.com>
2013-06-24 10:35:51 +0100

    added add, subtract and toggle convenience methods

---
+++ src/Ractive.prototype/add-subtract-toggle.js
@@ -0,0 +1,51 @@
+(function ( proto ) {
+
+	var add = function ( root, keypath, d ) {
+		var value;
+
+		if ( typeof keypath !== 'string' || !isNumeric( d ) ) {
+			if ( root.debug ) {
+				throw new Error( 'Bad arguments' );
+			}
+			return;
+		}
+
+		value = root.get( keypath );
+
+		if ( value === undefined ) {
+			value = 0;
+		}
+
+		if ( !isNumeric( value ) ) {
+			if ( root.debug ) {
+				throw new Error( 'Cannot add to a non-numeric value' );
+			}
+			return;
+		}
+
+		root.set( keypath, value + d );
+	};
+
+	proto.add = function ( keypath, d ) {
+		add( this, keypath, ( d === undefined ? 1 : d ) );
+	};
+
+	proto.subtract = function ( keypath, d ) {
+		add( this, keypath, ( d === undefined ? -1 : -d ) );
+	};
+
+	proto.toggle = function ( keypath ) {
+		var value;
+
+		if ( typeof keypath !== 'string' ) {
+			if ( this.debug ) {
+				throw new Error( 'Bad arguments' );
+			}
+			return;
+		}
+
+		value = this.get( keypath );
+		this.set( keypath, !value );
+	};
+
+}( proto ));
