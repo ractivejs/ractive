d4ecc12528ea092dd30d2d8f81ca950558157142
Rich Harris <richard.a.harris@gmail.com>
2013-07-09 15:32:56 +0100

    removed render method from API

---
+++ src/Ractive.prototype/helpers/render.js
@@ -0,0 +1,34 @@
+// Render instance to element specified here or at initialization
+render = function ( ractive, options ) {
+	var el, transitionManager;
+
+	el = ( options.el ? getEl( options.el ) : ractive.el );
+
+	// Clear the element, unless `append` is `true`
+	if ( el && !options.append ) {
+		el.innerHTML = '';
+	}
+
+	ractive._transitionManager = transitionManager = makeTransitionManager( ractive, options.complete );
+
+	// Render our *root fragment*
+	ractive.fragment = new DomFragment({
+		descriptor: ractive.template,
+		root: ractive,
+		owner: ractive, // saves doing `if ( ractive.parent ) { /*...*/ }` later on
+		parentNode: el
+	});
+
+	if ( el ) {
+		el.appendChild( ractive.fragment.docFrag );
+	}
+	
+	ractive.ready = true;
+
+	// transition manager has finished its work
+	ractive._transitionManager = null;
+	transitionManager.ready = true;
+	if ( !transitionManager.active ) {
+		transitionManager.complete();
+	}
+};
