c6844663843e097ab592edb4b78e0fdb021f2300
Rich Harris <richard.a.harris@gmail.com>
2013-07-16 17:50:29 +0100

    tweaked addEventListener polyfill

--- src/legacy.js
+++ src/legacy.js
@@ -1,4 +1,4 @@
-(function ( doc ) {
+(function ( win, doc ) {
 
 	// Shims for older browsers
 
@@ -92,60 +92,75 @@
 
 	// https://gist.github.com/jonathantneal/2869388
 	// addEventListener polyfill IE6+
-	!window.addEventListener && (function (window, document) {
-		function Event(e, element) {
-			var instance = this;
+	if ( !win.addEventListener ) {
+		(function ( win, doc ) {
+			var Event, addEventListener, removeEventListener, head, style;
 
-			for (property in e) {
-				instance[property] = e[property];
-			}
+			Event = function ( e, element ) {
+				var property, instance = this;
 
-			instance.currentTarget =  element;
-			instance.target = e.srcElement || element;
-			instance.timeStamp = +new Date;
-
-			instance.preventDefault = function () {
-				e.returnValue = false;
-			};
-			instance.stopPropagation = function () {
-				e.cancelBubble = true;
-			};
-		}
-
-		function addEventListener(type, listener) {
-			var
-			element = this,
-			listeners = element.listeners = element.listeners || [],
-			index = listeners.push([listener, function (e) {
-				listener.call(element, new Event(e, element));
-			}]) - 1;
-
-			element.attachEvent('on' + type, listeners[index][1]);
-		}
-
-		function removeEventListener(type, listener) {
-			for (var element = this, listeners = element.listeners || [], length = listeners.length, index = 0; index < length; ++index) {
-				if (listeners[index][0] === listener) {
-					element.detachEvent('on' + type, listeners[index][1]);
+				for ( property in e ) {
+					instance[ property ] = e[ property ];
 				}
+
+				instance.currentTarget =  element;
+				instance.target = e.srcElement || element;
+				instance.timeStamp = +new Date();
+
+				instance.preventDefault = function () {
+					e.returnValue = false;
+				};
+
+				instance.stopPropagation = function () {
+					e.cancelBubble = true;
+				};
+			};
+
+			addEventListener = function ( type, listener ) {
+				var element = this, listeners, i;
+
+				listeners = element.listeners || ( element.listeners = [] );
+				i = listeners.length;
+				
+				listeners[i] = [ listener, function (e) {
+					listener.call( element, new Event( e, element ) );
+				}];
+
+				element.attachEvent( 'on' + type, listeners[i][1] );
+			};
+
+			removeEventListener = function ( type, listener ) {
+				var element = this, listeners, len, index;
+
+				if ( !element.listeners ) {
+					return;
+				}
+
+				listeners = element.listeners;
+				i = listeners.length;
+
+				while ( i-- ) {
+					if (listeners[i][0] === listener) {
+						element.detachEvent( 'on' + type, listeners[i][1] );
+					}
+				}
+			};
+
+			win.addEventListener = doc.addEventListener = addEventListener;
+			win.removeEventListener = doc.removeEventListener = removeEventListener;
+
+			if ( 'Element' in win ) {
+				Element.prototype.addEventListener = addEventListener;
+				Element.prototype.removeEventListener = removeEventListener;
+			} else {
+				head = doc.getElementsByTagName('head')[0];
+				style = doc.createElement('style');
+
+				head.insertBefore( style, head.firstChild );
+
+				style.styleSheet.cssText = '*{-ms-event-prototype:expression(!this.addEventListener&&(this.addEventListener=addEventListener)&&(this.removeEventListener=removeEventListener))}';
 			}
-		}
+		}( win, doc ));
+	}
 
-		window.addEventListener = document.addEventListener = addEventListener;
-		window.removeEventListener = document.removeEventListener = removeEventListener;
-
-		if ('Element' in window) {
-			Element.prototype.addEventListener    = addEventListener;
-			Element.prototype.removeEventListener = removeEventListener;
-		} else {
-			var
-			head = document.getElementsByTagName('head')[0],
-			style = document.createElement('style');
-
-			head.insertBefore(style, head.firstChild);
-
-			style.styleSheet.cssText = '*{-ms-event-prototype:expression(!this.addEventListener&&(this.addEventListener=addEventListener)&&(this.removeEventListener=removeEventListener))}';
-		}
-	})(window, document) && scrollBy(0, 0);
-
-}( document ));
+}( window, document ));
