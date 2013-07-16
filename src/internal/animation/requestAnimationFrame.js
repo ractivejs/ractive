db41dbfb2b02033ab810bca06dd44f35494790e9
Rich Harris <richard.a.harris@gmail.com>
2013-07-16 16:51:23 +0100

    tweak requestAnimationFrame stuff

--- src/internal/animation/requestAnimationFrame.js
+++ src/internal/animation/requestAnimationFrame.js
@@ -1,35 +1,30 @@
 // https://gist.github.com/paulirish/1579671
 (function( vendors, lastTime, global ) {
 	
-	var x;
+	var x, setTimeout;
 
 	if ( global.requestAnimationFrame ) {
 		requestAnimationFrame = global.requestAnimationFrame;
-		cancelAnimationFrame = global.cancelAnimationFrame;
 		return;
 	}
 
 	for ( x = 0; x < vendors.length && !requestAnimationFrame; ++x ) {
 		requestAnimationFrame = global[vendors[x]+'RequestAnimationFrame'];
-		cancelAnimationFrame = global[vendors[x]+'CancelAnimationFrame'] || global[vendors[x]+'CancelRequestAnimationFrame'];
 	}
 
 	if ( !requestAnimationFrame ) {
+		setTimeout = global.setTimeout;
+
 		requestAnimationFrame = function(callback) {
 			var currTime, timeToCall, id;
 			
 			currTime = Date.now();
 			timeToCall = Math.max( 0, 16 - (currTime - lastTime ) );
-			id = global.setTimeout( function() { callback(currTime + timeToCall); }, timeToCall );
+			id = setTimeout( function() { callback(currTime + timeToCall); }, timeToCall );
 			
 			lastTime = currTime + timeToCall;
 			return id;
 		};
 	}
-
-	if ( !cancelAnimationFrame ) {
-		cancelAnimationFrame = function( id ) {
-			global.clearTimeout( id );
-		};
-	}
+	
 }( ['ms', 'moz', 'webkit', 'o'], 0, global ));
