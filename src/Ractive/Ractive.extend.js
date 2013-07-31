dd82fb88c23df841059bc710fa3900164f4c6e3f
Rich Harris <richard.a.harris@gmail.com>
2013-07-31 08:49:48 +0100

    thanks jshint. That would have been embarrassing

--- src/Ractive/Ractive.extend.js
+++ src/Ractive/Ractive.extend.js
@@ -68,12 +68,13 @@
 	wrapMethod = function ( method, superMethod ) {
 		if ( /_super/.test( method ) ) {
 			return function () {
-				var _super = this._super;
+				var _super = this._super, result;
 				this._super = superMethod;
 
-				return method.apply( this, arguments );
+				result = method.apply( this, arguments );
 
 				this._super = _super;
+				return result;
 			};
 		}
 
