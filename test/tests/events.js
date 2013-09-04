2d7aee42e702ac2ecbb9d57ac0a32218481993fb
Rich Harris <richard.a.harris@gmail.com>
2013-09-04 08:16:43 -0700

    added test for #158, plus general tap event test

--- test/tests/events.js
+++ test/tests/events.js
@@ -321,4 +321,48 @@
 		t.equal( ractive.findAll( 'input' ).length, 2 );
 	});
 
+	test( 'Mousedown followed by click results in a tap event', function ( t ) {
+		var ractive, tapped;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<span id="test" on-tap="tap">tap me</span>'
+		});
+
+		ractive.on( 'tap', function () {
+			tapped = true;
+		});
+
+		t.equal( tapped, undefined );
+		simulant.fire( ractive.nodes.test, 'mousedown' );
+		simulant.fire( ractive.nodes.test, 'click' );
+		t.equal( tapped, true );
+	});
+
+	test( 'Pressing spacebar on a focused button results in a tap event', function ( t ) {
+		var ractive, node, tapped;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<button id="test" on-tap="tap">tap me</button>'
+		});
+
+		node = ractive.nodes.test;
+
+		ractive.on( 'tap', function ( event ) {
+			console.log( event );
+			tapped = true;
+		});
+
+		t.equal( tapped, undefined );
+
+		simulant.fire( node, 'keydown', { which: 32 });
+		t.equal( tapped, undefined );
+
+		node.focus();
+		t.equal( document.activeElement, node );
+		simulant.fire( node, 'keydown', { which: 32 });
+		t.equal( tapped, true );
+	});
+
 }());
