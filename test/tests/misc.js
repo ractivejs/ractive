3dc0e53c8530bbb0aa748efb3faeb6b7857ec293
Rich Harris <richard.a.harris@gmail.com>
2013-09-04 09:59:15 -0700

    tests for #155

--- test/tests/misc.js
+++ test/tests/misc.js
@@ -220,6 +220,151 @@
 		t.ok( compareHTML( fixture.innerHTML, '<span data-text="0:zero">0:zero</span><span data-text="1:one">1:one</span><span data-text="2:two">2:two</span><span data-text="3:THREE">3:THREE</span><span data-text="4:four">4:four</span>' ) );
 	});
 
+	test( 'If a select value with two-way binding has a selected option at render time, the model updates accordingly', function ( t ) {
+		var ractive;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<select value="{{color}}"><option value="red">red</option><option value="blue">blue</option><option value="green" selected>green</option></select>'
+		});
+
+		t.equal( ractive.get( 'color' ), 'green' );
+	});
+
+	test( 'If a select value with two-way binding has no selected option at render time, the model defaults to the top value', function ( t ) {
+		var ractive;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<select value="{{color}}"><option value="red">red</option><option value="blue">blue</option><option value="green">green</option></select>'
+		});
+
+		t.equal( ractive.get( 'color' ), 'red' );
+	});
+
+	test( 'If the value of a select is specified in the model, it overrides the markup', function ( t ) {
+		var ractive;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<select value="{{color}}"><option value="red">red</option><option id="blue" value="blue">blue</option><option id="green" value="green" selected>green</option></select>',
+			data: { color: 'blue' }
+		});
+
+		t.equal( ractive.get( 'color' ), 'blue' );
+		t.ok( ractive.nodes.blue.selected );
+		t.ok( !ractive.nodes.green.selected );
+	});
+
+	test( 'If a multiple select value with two-way binding has a selected option at render time, the model updates accordingly', function ( t ) {
+		var ractive;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<select value="{{colors}}" multiple><option value="red">red</option><option value="blue" selected>blue</option><option value="green" selected>green</option></select>'
+		});
+
+		t.deepEqual( ractive.get( 'colors' ), [ 'blue', 'green' ] );
+	});
+
+	test( 'If a multiple select value with two-way binding has no selected option at render time, the model defaults to an empty array', function ( t ) {
+		var ractive;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<select value="{{colors}}" multiple><option value="red">red</option><option value="blue">blue</option><option value="green">green</option></select>'
+		});
+
+		t.deepEqual( ractive.get( 'colors' ), [] );
+	});
+
+	test( 'If the value of a multiple select is specified in the model, it overrides the markup', function ( t ) {
+		var ractive;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<select value="{{colors}}" multiple><option id="red" value="red">red</option><option id="blue" value="blue">blue</option><option id="green" value="green" selected>green</option></select>',
+			data: { colors: [ 'red', 'green' ] }
+		});
+
+		t.deepEqual( ractive.get( 'colors' ), [ 'red', 'green' ] );
+		t.ok( ractive.nodes.red.selected );
+		t.ok( !ractive.nodes.blue.selected );
+		t.ok( ractive.nodes.green.selected );
+	});
+
+	test( 'The model updates to reflect which checkbox inputs are checked at render time', function ( t ) {
+		var ractive;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: 'b<input id="red" type="checkbox" name="{{colors}}" value="red"><input id="green" type="checkbox" name="{{colors}}" value="blue" checked><input id="blue" type="checkbox" name="{{colors}}" value="green" checked>'
+		});
+
+		t.deepEqual( ractive.get( 'colors' ), [ 'blue', 'green' ] );
+		t.ok( !ractive.nodes.red.checked );
+		t.ok( ractive.nodes.blue.checked );
+		t.ok( ractive.nodes.green.checked );
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<input id="red" type="checkbox" name="{{colors}}" value="red"><input id="green" type="checkbox" name="{{colors}}" value="blue"><input id="blue" type="checkbox" name="{{colors}}" value="green">'
+		});
+
+		t.deepEqual( ractive.get( 'colors' ), [] );
+		t.ok( !ractive.nodes.red.checked );
+		t.ok( !ractive.nodes.blue.checked );
+		t.ok( !ractive.nodes.green.checked );
+	});
+
+	test( 'The model overrides which checkbox inputs are checked at render time', function ( t ) {
+		var ractive;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<input id="red" type="checkbox" name="{{colors}}" value="red"><input id="blue" type="checkbox" name="{{colors}}" value="blue" checked><input id="green" type="checkbox" name="{{colors}}" value="green" checked>',
+			data: { colors: [ 'red', 'blue' ] }
+		});
+
+		t.deepEqual( ractive.get( 'colors' ), [ 'red', 'blue' ] );
+		t.ok( ractive.nodes.red.checked );
+		t.ok( ractive.nodes.blue.checked );
+		t.ok( !ractive.nodes.green.checked );
+	});
+
+	test( 'The model updates to reflect which radio input is checked at render time', function ( t ) {
+		var ractive;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<input type="radio" name="{{color}}" value="red"><input type="radio" name="{{color}}" value="blue" checked><input type="radio" name="{{color}}" value="green">'
+		});
+
+		t.deepEqual( ractive.get( 'color' ), 'blue' );
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<input type="radio" name="{{color}}" value="red"><input type="radio" name="{{color}}" value="blue"><input type="radio" name="{{color}}" value="green">'
+		});
+
+		t.deepEqual( ractive.get( 'color' ), undefined );
+	});
+
+	test( 'The model overrides which radio input is checked at render time', function ( t ) {
+		var ractive;
+
+		ractive = new Ractive({
+			el: fixture,
+			template: '<input id="red" type="radio" name="{{color}}" value="red"><input id="blue" type="radio" name="{{color}}" value="blue" checked><input id="green" type="radio" name="{{color}}" value="green">',
+			data: { color: 'green' }
+		});
+
+		t.deepEqual( ractive.get( 'color' ), 'green' );
+		t.ok( !ractive.nodes.red.checked );
+		t.ok( !ractive.nodes.blue.checked );
+		t.ok( ractive.nodes.green.checked );
+	});
+
 	// These tests run fine in the browser but not in PhantomJS. WTF I don't even.
 	// Anyway I can't be bothered to figure it out right now so I'm just commenting
 	// these out so it will build
