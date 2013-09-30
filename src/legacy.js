2ab387e67551fbc0537db3f9043082d2a6317311
Rich Harris <richard.a.harris@gmail.com>
2013-09-30 16:42:45 +0100

    polyfill Object.keys

--- src/legacy.js
+++ src/legacy.js
@@ -28,6 +28,49 @@
 		};
 	}
 
+
+	// Polyfill for Object.keys
+	// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Object/keys
+	if ( !Object.keys ) {
+		Object.keys = (function () {
+			var hasOwnProperty = Object.prototype.hasOwnProperty,
+				hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+				dontEnums = [
+					'toString',
+					'toLocaleString',
+					'valueOf',
+					'hasOwnProperty',
+					'isPrototypeOf',
+					'propertyIsEnumerable',
+					'constructor'
+				],
+				dontEnumsLength = dontEnums.length;
+
+			return function ( obj ) {
+				if ( typeof obj !== 'object' && typeof obj !== 'function' || obj === null ) {
+					throw new TypeError( 'Object.keys called on non-object' );
+				}
+
+				var result = [];
+
+				for ( var prop in obj ) {
+					if ( hasOwnProperty.call( obj, prop ) ){
+						result.push( prop );
+					}
+				}
+
+				if ( hasDontEnumBug ) {
+					for ( var i=0; i < dontEnumsLength; i++ ) {
+						if ( hasOwnProperty.call( obj, dontEnums[i] ) ){
+							result.push( dontEnums[i] );
+						}
+					}
+				}
+				return result;
+			};
+		}());
+	}
+
 	
 	// Array extras
 	if ( !Array.prototype.indexOf ) {
