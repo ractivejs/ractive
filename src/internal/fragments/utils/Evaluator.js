bca5585e9590f1a73bde11a4e80df6d593572ba2
Rich Harris <richard.a.harris@gmail.com>
2013-10-01 16:35:02 +0100

    neater dependency tracking - anything retrieved with ractive.get() inside a function used in an expression is treated as a dependency for said expression. relevant to #34

--- src/internal/fragments/utils/Evaluator.js
+++ src/internal/fragments/utils/Evaluator.js
@@ -1,12 +1,13 @@
 (function ( cache ) {
 
-	var Reference, getFunctionFromString, thisPattern, wrapFunction;
+	var Reference, SoftReference, getFunctionFromString, thisPattern, wrapFunction;
 
 	Evaluator = function ( root, keypath, functionStr, args, priority ) {
 		var i, arg;
 
 		this.root = root;
 		this.keypath = keypath;
+		this.priority = priority;
 
 		this.dependants = 0;
 
@@ -123,6 +124,38 @@
 				this.update();
 				this.deferred = false;
 			}
+		},
+
+		updateSoftDependencies: function ( softDeps ) {
+			var i, keypath, ref;
+
+			if ( !this.softRefs ) {
+				this.softRefs = [];
+			}
+
+			// teardown any references that are no longer relevant
+			i = this.softRefs.length;
+			while ( i-- ) {
+				ref = this.softRefs[i];
+				if ( !softDeps[ ref.keypath ] ) {
+					this.softRefs.splice( i, 1 );
+					this.softRefs[ ref.keypath ] = false;
+					ref.teardown();
+				}
+			}
+
+			// add references for any new soft dependencies
+			i = softDeps.length;
+			while ( i-- ) {
+				keypath = softDeps[i];
+				if ( !this.softRefs[ keypath ] ) {
+					ref = new SoftReference( this.root, keypath, this );
+					this.softRefs[ this.softRefs.length ] = ref;
+					this.softRefs[ keypath ] = true;
+				}
+			}
+
+			this.selfUpdating = ( this.refs.length + this.softRefs.length <= 1 );
 		}
 	};
 
@@ -140,7 +173,7 @@
 		value = root.get( keypath );
 
 		if ( typeof value === 'function' ) {
-			value = value._wrapped || wrapFunction( value, root );
+			value = value._wrapped || wrapFunction( value, root, evaluator );
 		}
 
 		this.value = evaluator.values[ argNum ] = value;
@@ -153,13 +186,38 @@
 			var value = this.root.get( this.keypath );
 
 			if ( typeof value === 'function' && !value._nowrap ) {
-				value = value[ '_' + this.root._guid ] || wrapFunction( value, this.root );
+				value = value[ '_' + this.root._guid ] || wrapFunction( value, this.root, this.evaluator );
 			}
 
 			if ( !isEqual( value, this.value ) ) {
 				this.evaluator.values[ this.argNum ] = value;
 				this.evaluator.bubble();
 
+				this.value = value;
+			}
+		},
+
+		teardown: function () {
+			unregisterDependant( this );
+		}
+	};
+
+	SoftReference = function ( root, keypath, evaluator ) {
+		this.root = root;
+		this.keypath = keypath;
+		this.priority = evaluator.priority;
+
+		this.evaluator = evaluator;
+
+		registerDependant( this );
+	};
+
+	SoftReference.prototype = {
+		update: function () {
+			var value = this.root.get( this.keypath );
+
+			if ( !isEqual( value, this.value ) ) {
+				this.evaluator.bubble();
 				this.value = value;
 			}
 		},
@@ -192,7 +250,7 @@
 
 	thisPattern = /this/;
 
-	wrapFunction = function ( fn, ractive ) {
+	wrapFunction = function ( fn, ractive, evaluator ) {
 		var prop;
 
 		// if the function doesn't refer to `this`, we don't need
@@ -207,7 +265,32 @@
 		// otherwise, we do
 		defineProperty( fn, '_' + ractive._guid, {
 			value: function () {
-				return fn.apply( ractive, arguments );
+				var originalGet, result, softDependencies;
+
+				originalGet = ractive.get;
+				ractive.get = function ( keypath ) {
+					if ( !softDependencies ) {
+						softDependencies = [];
+					}
+
+					if ( !softDependencies[ keypath ] ) {
+						softDependencies[ softDependencies.length ] = keypath;
+						softDependencies[ keypath ] = true;
+					}
+					
+					return originalGet.call( ractive, keypath );
+				};
+				
+				result = fn.apply( ractive, arguments );
+				
+				if ( softDependencies ) {
+					evaluator.updateSoftDependencies( softDependencies );
+				}
+
+				// reset
+				ractive.get = originalGet;
+				
+				return result;
 			},
 			writable: true
 		});
