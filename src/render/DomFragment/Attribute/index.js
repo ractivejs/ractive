fc828cf55223da3541df9d15039662ca17c60a19
Rich Harris <richard.a.harris@gmail.com>
2013-11-10 10:40:24 +0000

    fixes #252 (and hopefully #248...)

--- src/render/DomFragment/Attribute/index.js
+++ src/render/DomFragment/Attribute/index.js
@@ -113,7 +113,7 @@ define([
 			// if the fragment this attribute belongs to gets reassigned (as a result of
 			// as section being updated via an array shift, unshift or splice), this
 			// attribute needs to recognise that its keypath has changed
-			this.keypath = this.interpolator.keypath || this.interpolator.r; // TODO is this right? .r?
+			this.keypath = this.interpolator.keypath || this.interpolator.ref;
 
 			// if we encounter the special case described above, update the name attribute
 			if ( this.propertyName === 'name' ) {
