26248a9aab01a69f18dcaa29ed68295ecbac6a73
Rich Harris <richard.a.harris@gmail.com>
2013-11-11 12:30:05 +0000

    fixes #240 (and #237 in theory)

--- src/shared/attemptKeypathResolution.js
+++ src/shared/attemptKeypathResolution.js
@@ -2,24 +2,27 @@ define([ 'shared/resolveRef' ], function ( resolveRef ) {
 	
 	'use strict';
 
+	var push = Array.prototype.push;
+
 	return function ( ractive ) {
-		var i, unresolved, keypath;
+		var unresolved, keypath, leftover;
 
 		// See if we can resolve any of the unresolved keypaths (if such there be)
-		i = ractive._pendingResolution.length;
-		while ( i-- ) { // Work backwards, so we don't go in circles!
-			unresolved = ractive._pendingResolution.splice( i, 1 )[0];
-
+		while ( unresolved = ractive._pendingResolution.pop() ) {
 			keypath = resolveRef( ractive, unresolved.ref, unresolved.contextStack );
+		
 			if ( keypath !== undefined ) {
 				// If we've resolved the keypath, we can initialise this item
 				unresolved.resolve( keypath );
 
 			} else {
-				// If we can't resolve the reference, add to the back of
-				// the queue (this is why we're working backwards)
-				ractive._pendingResolution[ ractive._pendingResolution.length ] = unresolved;
+				// If we can't resolve the reference, try again next time
+				( leftover || ( leftover = [] ) ).push( unresolved );
 			}
+		}
+
+		if ( leftover ) {
+			push.apply( ractive._pendingResolution, leftover );
 		}
 	};
 
