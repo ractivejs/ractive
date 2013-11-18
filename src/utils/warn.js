3aefcc23145954443d2924b6c871cf2cdb0fb1b3
Rich Harris <richard.a.harris@gmail.com>
2013-11-18 17:32:23 +0000

    [sigh] IE9 bugfix

--- src/utils/warn.js
+++ src/utils/warn.js
@@ -3,7 +3,7 @@ define( function () {
 	
 	'use strict';
 
-	if ( typeof console !== undefined && console.warn ) {
+	if ( typeof console !== undefined && typeof console.warn === 'function' && typeof console.warn.apply === 'function' ) {
 		return function () {
 			console.warn.apply( console, arguments );
 		};
