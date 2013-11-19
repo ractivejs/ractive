0861b30ce0a9f1a18a1406af0a4d7edfa78ff597
Rich Harris <richard.a.harris@gmail.com>
2013-11-19 22:34:14 +0000

    prevent smart updates from causing chaos when doing array.splice( start, x ) where x > array.length - start

--- src/render/DomFragment/Section.js
+++ src/render/DomFragment/Section.js
@@ -123,6 +123,11 @@ define([
 			addedItems = Math.max( 0, args.length - 2 );
 			removedItems = ( args[1] !== undefined ? args[1] : this.length - start );
 
+			// It's possible to do e.g. [ 1, 2, 3 ].splice( 2, 2 ) - i.e. the second argument
+			// means removing more items from the end of the array than there are. In these
+			// cases we need to curb JavaScript's enthusiasm or we'll get out of sync
+			removedItems = Math.min( removedItems, this.length - start );
+
 			balance = addedItems - removedItems;
 
 			if ( !balance ) {
