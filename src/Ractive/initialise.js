b6ebb3bc3f220a64669b9279393c7e898f3a6607
Rich Harris <richard.a.harris@gmail.com>
2013-11-21 12:11:04 +0000

    linting

--- src/Ractive/initialise.js
+++ src/Ractive/initialise.js
@@ -74,7 +74,8 @@ define([
 				value: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 					var r, v;
 
-					r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+					r = Math.random()*16|0;
+					v = ( c == 'x' ? r : (r&0x3|0x8) );
 					return v.toString(16);
 				})
 			},
