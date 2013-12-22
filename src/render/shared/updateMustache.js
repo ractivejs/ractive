dcf25146f3e9abbe0450fbd186917fdeb0af3b5c
Rich Harris <richard.a.harris@gmail.com>
2013-12-22 09:52:34 -0500

    within templates, references to wrapped objects should resolve to the adapted value, not the original

--- src/render/shared/updateMustache.js
+++ src/render/shared/updateMustache.js
@@ -3,7 +3,15 @@ define([ 'utils/isEqual' ], function ( isEqual ) {
 	'use strict';
 
 	return function () {
-		var value = this.root.get( this.keypath, true );
+		var wrapped, value;
+
+		// if we're getting a wrapped object, e.g. a promise, we want to
+		// get the adapted value, not the original
+		if ( wrapped = this.root._wrapped[ this.keypath ] ) {
+			value = wrapped.get();
+		} else {
+			value = this.root.get( this.keypath );
+		}
 
 		if ( !isEqual( value, this.value ) ) {
 			this.render( value );
