de9224d1609e3cc0932e24fecfcca7b35de015a4
Rich Harris <richard.a.harris@gmail.com>
2014-01-26 20:22:33 -0500

    make clone module general-purpose

--- src/utils/clone.js
+++ src/utils/clone.js
@@ -1,9 +1,23 @@
-define( function () {
+define([
+	'utils/isArray'
+], function (
+	isArray
+) {
 
 	'use strict';
 
 	return function ( source ) {
-		var target = {}, key;
+		var target, key;
+
+		if ( !source || typeof source !== 'object' ) {
+			return source;
+		}
+
+		if ( isArray( source ) ) {
+			return source.slice();
+		}
+
+		target = {};
 
 		for ( key in source ) {
 			if ( source.hasOwnProperty( key ) ) {
