9538c9003afd6dc87e7fdafcdaf8d2f817d21197
Rich Harris <richard.a.harris@gmail.com>
2014-01-29 18:49:46 -0500

    add global state helpers

---
+++ src/state/deferred/transitions.js
@@ -0,0 +1,11 @@
+define([
+
+], function (
+
+) {
+
+	'use strict';
+
+	return [];
+
+});
---
+++ src/state/pendingResolution.js
@@ -0,0 +1,46 @@
+define([
+	'utils/removeFromArray',
+	'shared/resolveRef'
+], function (
+	removeFromArray,
+	resolveRef
+) {
+
+	'use strict';
+
+	var pendingResolution = [];
+
+	pendingResolution.check = function () {
+		var clone, unresolved, keypath;
+
+		if ( !pendingResolution.length ) {
+			return;
+		}
+
+		clone = pendingResolution.splice( 0 );
+
+		// See if we can resolve any of the unresolved keypaths (if such there be)
+		while ( unresolved = clone.pop() ) {
+			if ( unresolved.keypath ) {
+				continue; // it did resolve after all
+			}
+
+			keypath = resolveRef( unresolved.root, unresolved.ref, unresolved.contextStack );
+
+			if ( keypath !== undefined ) {
+				// If we've resolved the keypath, we can initialise this item
+				unresolved.resolve( keypath );
+			} else {
+				// If we can't resolve the reference, try again next time
+				pendingResolution.push( unresolved );
+			}
+		}
+	};
+
+	pendingResolution.remove = function ( thing ) {
+		removeFromArray( pendingResolution, thing );
+	};
+
+	return pendingResolution;
+
+});
