ractivejs/ractive

commit d2953959f178fda7d82c1e4cae12981c70d39154

guard against component bindings causing infinite loops

src/shared/createComponentBinding.js
@@ -21,42 +21,35 @@ define([
 		this.otherKeypath = otherKeypath;
 
 		registerDependant( this );
+
+		this.value = this.root.get( this.keypath );
 	};
 
 	Binding.prototype = {
-		init: function ( propagate ) {
-			var value = this.root.get( this.keypath );
-
-			// Data should propagate from child to parent
-			if ( propagate && value !== undefined ) {
-				this.update();
-			} else {
-				this.value = value;
-			}
-		},
-
 		update: function () {
 			var value;
 
 			// Only *you* can prevent infinite loops
-			if ( this.counterpart && this.counterpart.setting ) {
+			if ( this.updating || this.counterpart && this.counterpart.updating ) {
 				return;
 			}
+
+			this.updating = true;
 
 			value = this.root.get( this.keypath );
 
 			// Is this a smart array update? If so, it'll update on its
 			// own, we shouldn't do anything
-			if ( isArray( value ) && value._ractive && value._ractive.setting ) {
+			if ( isArray( value ) && value._ractive && value._ractive.updating ) {
 				return;
 			}
 
 			if ( !isEqual( value, this.value ) ) {
-				this.setting = true;
 				this.otherInstance.set( this.otherKeypath, value );
 				this.value = value;
-				this.setting = false;
 			}
+
+			this.updating = false;
 		},
 
 		teardown: function () {
@@ -82,7 +75,6 @@ define([
 		priority = component.parentFragment.priority;
 
 		parentToChildBinding = new Binding( parentInstance, parentKeypath, childInstance, childKeypath, priority );
-		parentToChildBinding.init();
 		bindings.push( parentToChildBinding );
 
 		if ( childInstance.twoway ) {
@@ -91,9 +83,6 @@ define([
 
 			parentToChildBinding.counterpart = childToParentBinding;
 			childToParentBinding.counterpart = parentToChildBinding;
-
-			// propagate child data upwards, if it exists already
-			//childToParentBinding.init( true );
 		}
 	};
 

commit c3535f321899020f4e1767a5fca55d66e3f698bd

fix bug introduced with d2953959f178fda7d82c1e4cae12981c70d39154

src/shared/createComponentBinding.js
@@ -34,22 +34,20 @@ define([
 				return;
 			}
 
-			this.updating = true;
-
 			value = this.root.get( this.keypath );
 
 			// Is this a smart array update? If so, it'll update on its
 			// own, we shouldn't do anything
-			if ( isArray( value ) && value._ractive && value._ractive.updating ) {
+			if ( isArray( value ) && value._ractive && value._ractive.setting ) {
 				return;
 			}
 
 			if ( !isEqual( value, this.value ) ) {
+				this.updating = true;
 				this.otherInstance.set( this.otherKeypath, value );
 				this.value = value;
+				this.updating = false;
 			}
-
-			this.updating = false;
 		},
 
 		teardown: function () {