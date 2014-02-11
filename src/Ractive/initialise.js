ractivejs/ractive

commit 411d04bb609da07dbdcfc2b2778946cbb1f81e5b

only call component init methods after they have entered the DOM. Potentially breaking change

src/extend/initChildInstance.js
@@ -1,9 +1,11 @@
 define([
 	'config/initOptions',
+	'state/scheduler',
 	'extend/wrapMethod',
 	'Ractive/initialise'
 ], function (
 	initOptions,
+	scheduler,
 	wrapMethod,
 	initialise
 ) {
@@ -29,7 +31,7 @@ define([
 		initialise( child, options );
 
 		if ( child.init ) {
-			child.init( options );
+			scheduler.addComponent({ instance: child, options: options });
 		}
 	};
 

src/state/scheduler.js
@@ -22,6 +22,7 @@ define([
 		transitions = [],
 		observers = [],
 		attributes = [],
+		components = [],
 
 		evaluators = [],
 		selectValues = [],
@@ -78,6 +79,10 @@ define([
 			attributes.push( attribute );
 		},
 
+		addComponent: function ( component ) {
+			components.push( component );
+		},
+
 		// changes that may cause additional changes...
 		addEvaluator: function ( evaluator ) {
 			dirty = true;
@@ -117,6 +122,10 @@ define([
 		if ( toFocus ) {
 			toFocus.focus();
 			toFocus = null;
+		}
+
+		while ( thing = components.pop() ) {
+			thing.instance.init( thing.options );
 		}
 
 		while ( thing = attributes.pop() ) {

commit 604b544b2e34e769d25555468905bf646d74847f

fixes bug introduced with 411d04bb609da07dbdcfc2b2778946cbb1f81e5b whereby init methods became unreliable

src/Ractive/initialise.js
@@ -118,7 +118,10 @@ define([
 			_liveComponentQueries: { value: [] },
 
 			// nodes to detach at the end of a mutation
-			_detachQueue: { value: [] }
+			_detachQueue: { value: [] },
+
+			// components to init at the end of a mutation
+			_childInitQueue: { value: [] }
 		});
 
 		// If this is a component, store a reference to the parent

src/Ractive/prototype/render.js
@@ -49,9 +49,29 @@ define([
 		this.rendered = true;
 		scheduler.end();
 
+		// If this is a top-level instance (i.e. it was created with `var ractive = new Ractive()`,
+		// or `widget = new Widget()` etc), we need to initialise any child components now that
+		// they're in the DOM
+		if ( !this._parent ) {
+			initChildren( this );
+		}
+
 		// transition manager has finished its work
 		this._transitionManager = null;
 		transitionManager.init();
 	};
+
+	function initChildren ( instance ) {
+		var child;
+
+		while ( child = instance._childInitQueue.pop() ) {
+			if ( child.instance.init ) {
+				child.instance.init( child.options );
+			}
+
+			// now do the same for grandchildren, etc
+			initChildren( child.instance );
+		}
+	}
 
 });

src/extend/initChildInstance.js
@@ -30,8 +30,15 @@ define([
 
 		initialise( child, options );
 
-		if ( child.init ) {
-			scheduler.addComponent({ instance: child, options: options });
+		// If this is an inline component (i.e. NOT created with `var widget = new Widget()`,
+		// but rather `<widget/>` or similar), we don't want to call the `init` method until
+		// the component is in the DOM. That makes it easier for component authors to do stuff
+		// like `this.width = this.find('*').clientWidth` or whatever without using
+		// ugly setTimeout hacks.
+		if ( options._parent && !options._parent.rendered ) {
+			options._parent._childInitQueue.push({ instance: child, options: options });
+		} else if ( child.init ) {
+			child.init( options );
 		}
 	};
 

src/state/scheduler.js
@@ -22,7 +22,6 @@ define([
 		transitions = [],
 		observers = [],
 		attributes = [],
-		components = [],
 
 		evaluators = [],
 		selectValues = [],
@@ -79,10 +78,6 @@ define([
 			attributes.push( attribute );
 		},
 
-		addComponent: function ( component ) {
-			components.push( component );
-		},
-
 		// changes that may cause additional changes...
 		addEvaluator: function ( evaluator ) {
 			dirty = true;
@@ -122,10 +117,6 @@ define([
 		if ( toFocus ) {
 			toFocus.focus();
 			toFocus = null;
-		}
-
-		while ( thing = components.pop() ) {
-			thing.instance.init( thing.options );
 		}
 
 		while ( thing = attributes.pop() ) {