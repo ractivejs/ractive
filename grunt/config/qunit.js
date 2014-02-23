7b6f89b4f5a057e8e8012895ca5f35350d2096a1
Rich Harris <richard.a.harris@gmail.com>
2014-02-23 15:49:39 -0500

    build qunit task config dynamically

--- grunt/config/qunit.js
+++ grunt/config/qunit.js
@@ -1,16 +1,19 @@
-module.exports = {
-	all:        [ 'test/tests/index.html'      ],
-	parse:      [ 'test/tests/parse.html'      ],
-	render:     [ 'test/tests/render.html'     ],
-	mustache:   [ 'test/tests/mustache.html'   ],
-	events:     [ 'test/tests/events.html'     ],
-	misc:       [ 'test/tests/misc.html'       ],
-	components: [ 'test/tests/components.html' ],
-	merge:      [ 'test/tests/merge.html'      ],
-	observe:    [ 'test/tests/observe.html'    ],
-	find:       [ 'test/tests/find.html'       ],
-	arrays:     [ 'test/tests/arrays.html'     ],
-	options: {
-		timeout: 30000
-	}
+module.exports = function ( grunt ) {
+
+	'use strict';
+
+	var qunitConfig = {};
+
+	grunt.file.expand( 'test/tests/*.html' ).forEach( function ( path ) {
+		var testName = /test\/tests\/(.+)\.html/.exec( path )[1];
+
+		if ( testName === 'index' ) {
+			testName = 'all';
+		}
+
+		qunitConfig[ testName ] = path;
+	});
+
+	return qunitConfig;
+
 };
