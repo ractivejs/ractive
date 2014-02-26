919f3f237b916664190b8ec2fa982e7de7f479b1
Rich Harris <richard.a.harris@gmail.com>
2014-02-25 21:05:23 -0500

    fixes #451 - functions should behave as objects for purposes of reference resolution and section context-setting

--- src/shared/resolveRef.js
+++ src/shared/resolveRef.js
@@ -79,7 +79,7 @@ define([
 				parentValue = wrapped.get();
 			}
 
-			if ( typeof parentValue === 'object' && parentValue !== null && lastKey in parentValue ) {
+			if ( parentValue && ( typeof parentValue === 'object' || typeof parentValue === 'function' ) && lastKey in parentValue ) {
 				return context + '.' + ref;
 			}
 		} while ( fragment = fragment.parent );
