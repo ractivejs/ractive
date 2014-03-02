a048b865748f252522bb60524c65434815850379
Rich Harris <richard.a.harris@gmail.com>
2014-03-02 09:45:36 -0500

    fix two-way binding change propagation bug (#460)

--- src/render/DomFragment/Attribute/prototype/bind.js
+++ src/render/DomFragment/Attribute/prototype/bind.js
@@ -93,7 +93,9 @@ define([
 	// This is the handler for DOM events that would lead to a change in the model
 	// (i.e. change, sometimes, input, and occasionally click and keyup)
 	updateModel = function () {
+		runloop.start( this._ractive.root );
 		this._ractive.binding.update();
+		runloop.end();
 	};
 
 	getOptions = { evaluateWrapped: true };
