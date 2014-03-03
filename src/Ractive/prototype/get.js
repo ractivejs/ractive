959fb36ccbc89c542e8d23d5ef3764b5c57e1e88
Rich Harris <richard.a.harris@gmail.com>
2014-03-02 22:23:17 -0500

    prevent similar bugs from #464

--- src/Ractive/prototype/get.js
+++ src/Ractive/prototype/get.js
@@ -20,14 +20,14 @@ define([
 		value = get( this, keypath, options );
 
 		// capture the dependency, if we're inside an evaluator
-		if ( this._captured && !this._captured[ keypath ] ) {
+		if ( this._captured && ( this._captured[ keypath ] !== true ) ) {
 			this._captured.push( keypath );
 			this._captured[ keypath ] = true;
 
 			// if we couldn't resolve the keypath, we need to make it as a failed
 			// lookup, so that the evaluator updates correctly once we CAN
 			// resolve the keypath
-			if ( value === undefined && !this._unresolvedImplicitDependencies[ keypath ] ) {
+			if ( value === undefined && ( this._unresolvedImplicitDependencies[ keypath ] !== true ) ) {
 				new UnresolvedImplicitDependency( this, keypath );
 			}
 		}
