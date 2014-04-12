caf4a17b034b2e2bb6895638258b809ac7d974e0
Rich Harris <richard.a.harris@gmail.com>
2014-04-12 17:14:55 -0400

    ...

--- scripts/deploy-edge-to-cdn.sh
+++ scripts/deploy-edge-to-cdn.sh
@@ -4,16 +4,16 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "dev" ]; then
 	echo "Deploying edge version to CDN..."
 
-	git clone https://github.com/ractivejs/cdn.ractivejs.org.git cdn
+	git clone https://Rich-Harris:${GH_TOKEN}@${GH_REF} gh-pages
 	git checkout -b gh-pages origin/gh-pages
 
-	rm -r cdn/edge
-	cp -r build/ cdn/edge
+	rm -r gh-pages/edge
+	cp -r build/ gh-pages/edge
 
-	( cd cdn
+	( cd gh-pages
 
 		git remote rm origin
-		git remote add origin https://Rich-Harris:${GH_TOKEN}@github.com/ractivejs/cdn.ractivejs.org.git
+		git remote add origin https://Rich-Harris:${GH_TOKEN}@${GH_REF}
 
 		git add -A
 		git commit -m "Updated edge version"
