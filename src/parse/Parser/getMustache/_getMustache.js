a8371a76f9bf951dbf3e2d7acb15afb9bf00c144
Rich Harris <richard.a.harris@gmail.com>
2014-04-13 21:08:06 -0400

    remove MustacheStub

--- src/parse/Parser/getMustache/_getMustache.js
+++ src/parse/Parser/getMustache/_getMustache.js
@@ -1,20 +1,47 @@
 define([
 	'config/types',
-	'parse/Parser/getMustache/MustacheStub'
+	'parse/Parser/shared/KeypathExpressionStub',
+	'parse/Parser/shared/ExpressionStub'
 ], function (
 	types,
-	MustacheStub
+	KeypathExpressionStub,
+	ExpressionStub
 ) {
 
 	'use strict';
 
 	return function ( token ) {
+		var stub;
+
 		if ( token.type === types.MUSTACHE || token.type === types.TRIPLE ) {
 			if ( token.mustacheType === types.SECTION || token.mustacheType === types.INVERTED ) {
 				return null;
 			}
 
-			return new MustacheStub( token, this );
+			this.pos += 1;
+
+			stub = {
+				t: ( token.type === types.TRIPLE ? token.type : token.mustacheType )
+			};
+
+			if ( token.ref ) {
+				stub.r = token.ref;
+			}
+
+			if ( token.keypathExpression ) {
+				stub.kx = new KeypathExpressionStub( token.keypathExpression ).toJSON();
+			}
+
+			if ( token.expression ) {
+				stub.x = new ExpressionStub( token.expression ).toJSON();
+			}
+
+			// TEMP
+			Object.defineProperty( stub, 'toString', {
+				value: function () { return false; }
+			});
+
+			return stub;
 		}
 	};
 
