a40e919e2c8a3e86259d55cce09eccd5d2dd5934
Rich Harris <richard.a.harris@gmail.com>
2014-04-29 20:27:54 -0400

    nodeunit config can be simplified

--- grunt/config/nodeunit.js
+++ grunt/config/nodeunit.js
@@ -1,15 +1,3 @@
-module.exports = function ( grunt ) {
-
-	'use strict';
-
-	var nodeunitConfig = {};
-
-	grunt.file.expand( 'test/node/*.js' ).forEach( function ( path ) {
-		var testName = /test\/node\/(.+)\.js/.exec( path )[1];
-
-		nodeunitConfig[ testName ] = path;
-	});
-
-	return nodeunitConfig;
-
-};
+module.exports = {
+	all: 'test/node/*.js'
+}
