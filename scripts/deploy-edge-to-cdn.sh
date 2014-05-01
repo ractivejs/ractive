3f522c971aa5a80dae87c6ce6da773efab52de25
Rich Harris <richard.a.harris@gmail.com>
2014-04-30 22:54:32 -0400

    attempt to auto-deploy to build branch

--- scripts/deploy-edge-to-cdn.sh
+++ scripts/deploy-edge-to-cdn.sh
@@ -2,23 +2,41 @@
 
 # deploy script based on https://medium.com/philosophy-logic/53a8270e87db
 if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "dev" ]; then
-	echo "Deploying edge version to CDN..."
+	git config --global user.email "richard.a.harris+travis@gmail.com"
+	git config --global user.name "Travis-CI"
+
+	echo "Deploying to build branch"
+	git clone https://Rich-Harris:${GH_TOKEN}@${GH_REF} -b build build-branch
+	cp build/* build-branch
+
+	( cd build-branch
+		echo "Setting credentials..."
+		git remote rm origin
+		git remote add origin https://Rich-Harris:${GH_TOKEN}@${GH_REF}
+
+		echo "Adding files..."
+		git add -A
+		git commit -m "Updated edge version"
+
+		echo "Pushing to GitHub..."
+		git push --quiet 2> /dev/null
+
+		# TODO tags
+	)
+
+	echo "Deploying to CDN..."
 
 	echo "Cloning repo..."
-	git clone https://Rich-Harris:${GH_TOKEN}@${GH_REF} cdn
+	git clone https://Rich-Harris:${GH_TOKEN}@${CDN_REF} cdn
 
 	echo "Copying latest builds..."
 	rm -r cdn/edge
 	cp -r build/ cdn/edge
 
 	( cd cdn
-
 		echo "Setting credentials..."
 		git remote rm origin
-		git remote add origin https://Rich-Harris:${GH_TOKEN}@${GH_REF}
-
-		git config --global user.email "richard.a.harris+travis@gmail.com"
-		git config --global user.name "Travis-CI"
+		git remote add origin https://Rich-Harris:${GH_TOKEN}@${CDN_REF}
 
 		echo "Adding files..."
 		git add -A
