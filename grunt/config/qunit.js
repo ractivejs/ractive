ffce18347d4865e63fd697143fe79a026806639d
Rich Harris <richard.a.harris@gmail.com>
2014-05-14 21:57:40 -0400

    get test config from .js files rather than .html, which dont exist initially

--- grunt/config/qunit.js
+++ grunt/config/qunit.js
@@ -3,17 +3,19 @@ module.exports = function ( grunt ) {
 	'use strict';
 
 	var qunitConfig = {},
-		tests = grunt.template.process( '<%= tmpDir %>/test/tests/**/*.html' );
+		tests = grunt.template.process( 'test/modules/**/*.js' );
 
 	grunt.file.expand( tests ).forEach( function ( path ) {
-		var testName = /test\/tests\/(.+)\.html/.exec( path )[1];
+		var testName = /test\/modules\/(.+)\.js/.exec( path )[1];
 
 		if ( testName === 'index' ) {
 			testName = 'all';
 		}
 
-		qunitConfig[ testName.replace(/\//g, '-') ] = path;
+		qunitConfig[ testName.replace(/\//g, '-') ] = path.replace( '/modules/', 'tests' ).replace( '.js', '.html' );
 	});
+
+	qunitConfig.all = '<%= tmpDir %>/test/tests/index.html';
 
 	return qunitConfig;
 
