a8bedd7407f63de5539c4e7319452f2ccabf6194
Rich Harris <richard.a.harris@gmail.com>
2014-05-29 23:27:37 -0400

    prevent assignNewKeypath from reassigning a fragment context to undefined (#756)

--- src/virtualdom/items/shared/utils/assignNewKeypath.js
+++ src/virtualdom/items/shared/utils/assignNewKeypath.js
@@ -2,6 +2,11 @@ import startsWith from 'virtualdom/items/shared/utils/startsWith';
 import getNewKeypath from 'virtualdom/items/shared/utils/getNewKeypath';
 
 export default function assignNewKeypath ( target, property, oldKeypath, newKeypath ) {
-	if ( !target[property] || startsWith(target[property], newKeypath) ) { return; }
-	target[property] = getNewKeypath(target[property], oldKeypath, newKeypath);
+	var existingKeypath = target[ property ];
+
+	if ( !existingKeypath || startsWith( existingKeypath, newKeypath ) || !startsWith( existingKeypath, oldKeypath ) ) {
+		return;
+	}
+
+	target[ property ] = getNewKeypath( existingKeypath, oldKeypath, newKeypath );
 }
--- test/vendor/qunit-html.js
+++ test/vendor/qunit-html.js
@@ -61,7 +61,7 @@
 			i = node.attributes.length;
 			while ( i-- ) {
 				attr = node.attributes[i];
-				stub.attributes[ attr.name ] = attr.value || true;
+				stub.attributes[ attr.name ] = attr.value;
 			}
 
 			return stub;
