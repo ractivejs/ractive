4f92df89bda7603e0fe1ab9b0506021b2ce595f1
Rich Harris <richard.a.harris@gmail.com>
2014-05-30 10:53:51 -0400

    duplicate warnings are squelched by default. also, missing transition functions do not cause errors

--- src/utils/warn.js
+++ src/utils/warn.js
@@ -1,9 +1,17 @@
 /* global console */
-var warn;
+var warn, warned = {};
 
 if ( typeof console !== 'undefined' && typeof console.warn === 'function' && typeof console.warn.apply === 'function' ) {
-	warn = function () {
-		console.warn.apply( console, arguments );
+	warn = function ( message, allowDuplicates ) {
+		if ( !allowDuplicates ) {
+			if ( warned[ message ] ) {
+				return;
+			}
+
+			warned[ message ] = true;
+		}
+
+		console.warn( message );
 	};
 } else {
 	warn = function () {};
