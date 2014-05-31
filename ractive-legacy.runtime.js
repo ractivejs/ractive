019f1d5753ff18cc98e550ea70266be9a6ed46a3
Travis-CI <richard.a.harris+travis@gmail.com>
2014-05-31 23:23:06 +0000

    Updated edge version

--- ractive-legacy.runtime.js
+++ ractive-legacy.runtime.js
@@ -1,6 +1,6 @@
 /*
 	ractive-legacy.runtime.js v0.4.0
-	2014-05-30 - commit 053f13ea 
+	2014-05-31 - commit 0100326d 
 
 	http://ractivejs.org
 	http://twitter.com/RactiveJS
@@ -971,10 +971,107 @@
 		return upstreamChanges;
 	};
 
-	/* shared/notifyDependants.js */
-	var notifyDependants = function( circular ) {
+	/* shared/notifyPatternObservers.js */
+	var notifyPatternObservers = function() {
 
-		var get, lastKey = /[^\.]+$/, starMaps = {}, unwrap = {
+		var lastKey = /[^\.]+$/,
+			starMaps = {};
+		// TODO split into two functions? i.e. one for the top-level call, one for the cascade
+		return function notifyPatternObservers( ractive, registeredKeypath, actualKeypath, isParentOfChangedKeypath, isTopLevelCall ) {
+			var i, patternObserver, children, child, key, childActualKeypath, potentialWildcardMatches, cascade;
+			// First, observers that match patterns at the same level
+			// or higher in the tree
+			i = ractive._patternObservers.length;
+			while ( i-- ) {
+				patternObserver = ractive._patternObservers[ i ];
+				if ( patternObserver.regex.test( actualKeypath ) ) {
+					patternObserver.update( actualKeypath );
+				}
+			}
+			if ( isParentOfChangedKeypath ) {
+				return;
+			}
+			// If the changed keypath is 'foo.bar', we need to see if there are
+			// any pattern observer dependants of keypaths below any of
+			// 'foo.bar', 'foo.*', '*.bar' or '*.*' (e.g. 'foo.bar.*' or 'foo.*.baz' )
+			cascade = function( keypath ) {
+				if ( children = ractive._depsMap[ keypath ] ) {
+					i = children.length;
+					while ( i-- ) {
+						child = children[ i ];
+						// foo.*.baz
+						key = lastKey.exec( child )[ 0 ];
+						// 'baz'
+						childActualKeypath = actualKeypath ? actualKeypath + '.' + key : key;
+						// 'foo.bar.baz'
+						notifyPatternObservers( ractive, child, childActualKeypath );
+					}
+				}
+			};
+			if ( isTopLevelCall ) {
+				potentialWildcardMatches = getPotentialWildcardMatches( actualKeypath );
+				potentialWildcardMatches.forEach( cascade );
+			} else {
+				cascade( registeredKeypath );
+			}
+		};
+		// This function takes a keypath such as 'foo.bar.baz', and returns
+		// all the variants of that keypath that include a wildcard in place
+		// of a key, such as 'foo.bar.*', 'foo.*.baz', 'foo.*.*' and so on.
+		// These are then checked against the dependants map (ractive._depsMap)
+		// to see if any pattern observers are downstream of one or more of
+		// these wildcard keypaths (e.g. 'foo.bar.*.status')
+		function getPotentialWildcardMatches( keypath ) {
+			var keys, starMap, mapper, i, result, wildcardKeypath;
+			keys = keypath.split( '.' );
+			starMap = getStarMap( keys.length );
+			result = [];
+			mapper = function( star, i ) {
+				return star ? '*' : keys[ i ];
+			};
+			i = starMap.length;
+			while ( i-- ) {
+				wildcardKeypath = starMap[ i ].map( mapper ).join( '.' );
+				if ( !result[ wildcardKeypath ] ) {
+					result.push( wildcardKeypath );
+					result[ wildcardKeypath ] = true;
+				}
+			}
+			return result;
+		}
+		// This function returns all the possible true/false combinations for
+		// a given number - e.g. for two, the possible combinations are
+		// [ true, true ], [ true, false ], [ false, true ], [ false, false ].
+		// It does so by getting all the binary values between 0 and e.g. 11
+		function getStarMap( num ) {
+			var ones = '',
+				max, binary, starMap, mapper, i;
+			if ( !starMaps[ num ] ) {
+				starMap = [];
+				while ( ones.length < num ) {
+					ones += 1;
+				}
+				max = parseInt( ones, 2 );
+				mapper = function( digit ) {
+					return digit === '1';
+				};
+				for ( i = 0; i <= max; i += 1 ) {
+					binary = i.toString( 2 );
+					while ( binary.length < num ) {
+						binary = '0' + binary;
+					}
+					starMap[ i ] = Array.prototype.map.call( binary, mapper );
+				}
+				starMaps[ num ] = starMap;
+			}
+			return starMaps[ num ];
+		}
+	}();
+
+	/* shared/notifyDependants.js */
+	var notifyDependants = function( notifyPatternObservers, circular ) {
+
+		var get, unwrap = {
 			evaluateWrapped: true
 		};
 		circular.push( function() {
@@ -1049,97 +1146,7 @@
 				}
 			}
 		}
-		// TODO split into two functions? i.e. one for the top-level call, one for the cascade
-		function notifyPatternObservers( ractive, registeredKeypath, actualKeypath, isParentOfChangedKeypath, isTopLevelCall ) {
-			var i, patternObserver, children, child, key, childActualKeypath, potentialWildcardMatches, cascade;
-			// First, observers that match patterns at the same level
-			// or higher in the tree
-			i = ractive._patternObservers.length;
-			while ( i-- ) {
-				patternObserver = ractive._patternObservers[ i ];
-				if ( patternObserver.regex.test( actualKeypath ) ) {
-					patternObserver.update( actualKeypath );
-				}
-			}
-			if ( isParentOfChangedKeypath ) {
-				return;
-			}
-			// If the changed keypath is 'foo.bar', we need to see if there are
-			// any pattern observer dependants of keypaths below any of
-			// 'foo.bar', 'foo.*', '*.bar' or '*.*' (e.g. 'foo.bar.*' or 'foo.*.baz' )
-			cascade = function( keypath ) {
-				if ( children = ractive._depsMap[ keypath ] ) {
-					i = children.length;
-					while ( i-- ) {
-						child = children[ i ];
-						// foo.*.baz
-						key = lastKey.exec( child )[ 0 ];
-						// 'baz'
-						childActualKeypath = actualKeypath ? actualKeypath + '.' + key : key;
-						// 'foo.bar.baz'
-						notifyPatternObservers( ractive, child, childActualKeypath );
-					}
-				}
-			};
-			if ( isTopLevelCall ) {
-				potentialWildcardMatches = getPotentialWildcardMatches( actualKeypath );
-				potentialWildcardMatches.forEach( cascade );
-			} else {
-				cascade( registeredKeypath );
-			}
-		}
-		// This function takes a keypath such as 'foo.bar.baz', and returns
-		// all the variants of that keypath that include a wildcard in place
-		// of a key, such as 'foo.bar.*', 'foo.*.baz', 'foo.*.*' and so on.
-		// These are then checked against the dependants map (ractive._depsMap)
-		// to see if any pattern observers are downstream of one or more of
-		// these wildcard keypaths (e.g. 'foo.bar.*.status')
-		function getPotentialWildcardMatches( keypath ) {
-			var keys, starMap, mapper, i, result, wildcardKeypath;
-			keys = keypath.split( '.' );
-			starMap = getStarMap( keys.length );
-			result = [];
-			mapper = function( star, i ) {
-				return star ? '*' : keys[ i ];
-			};
-			i = starMap.length;
-			while ( i-- ) {
-				wildcardKeypath = starMap[ i ].map( mapper ).join( '.' );
-				if ( !result[ wildcardKeypath ] ) {
-					result.push( wildcardKeypath );
-					result[ wildcardKeypath ] = true;
-				}
-			}
-			return result;
-		}
-		// This function returns all the possible true/false combinations for
-		// a given number - e.g. for two, the possible combinations are
-		// [ true, true ], [ true, false ], [ false, true ], [ false, false ].
-		// It does so by getting all the binary values between 0 and e.g. 11
-		function getStarMap( num ) {
-			var ones = '',
-				max, binary, starMap, mapper, i;
-			if ( !starMaps[ num ] ) {
-				starMap = [];
-				while ( ones.length < num ) {
-					ones += 1;
-				}
-				max = parseInt( ones, 2 );
-				mapper = function( digit ) {
-					return digit === '1';
-				};
-				for ( i = 0; i <= max; i += 1 ) {
-					binary = i.toString( 2 );
-					while ( binary.length < num ) {
-						binary = '0' + binary;
-					}
-					starMap[ i ] = Array.prototype.map.call( binary, mapper );
-				}
-				starMaps[ num ] = starMap;
-			}
-			return starMaps[ num ];
-		}
-	}( circular );
+	}( notifyPatternObservers, circular );
 
 	/* shared/makeTransitionManager.js */
 	var makeTransitionManager = function( removeFromArray ) {
@@ -1650,14 +1657,14 @@
 	}( circular, isEqual, createBranch, clearCache, notifyDependants );
 
 	/* shared/get/arrayAdaptor/processWrapper.js */
-	var processWrapper = function( types, clearCache, notifyDependants, set, circular ) {
+	var processWrapper = function( types, clearCache, notifyDependants, notifyPatternObservers, set, circular ) {
 
 		var get;
 		circular.push( function() {
 			get = circular.get;
 		} );
 		return function( wrapper, array, methodName, spliceSummary ) {
-			var root, keypath, updateDependant, i, childKeypath, patternObservers;
+			var root, keypath, updateDependant, i, childKeypath;
 			root = wrapper.root;
 			keypath = wrapper.keypath;
 			root._changes.push( keypath );
@@ -1679,16 +1686,8 @@
 			}
 			// Propagate changes. First, pattern observers
 			if ( root._patternObservers.length ) {
-				patternObservers = root._patternObservers.filter( function( patternObserver ) {
-					return patternObserver.regex.test( keypath + '.x' );
-				} );
-				if ( patternObservers.length ) {
-					patternObservers.forEach( function( patternObserver ) {
-						var i;
-						for ( i = spliceSummary.rangeStart; i < spliceSummary.rangeEnd; i += 1 ) {
-							patternObserver.update( keypath + '.' + i );
-						}
-					} );
+				for ( i = spliceSummary.rangeStart; i < spliceSummary.rangeEnd; i += 1 ) {
+					notifyPatternObservers( root, keypath + '.' + i, keypath + '.' + i, false, true );
 				}
 			}
 			updateDependant = function( dependant ) {
@@ -1723,7 +1722,7 @@
 				notifyDependants( root, keypath + '.length', true );
 			}
 		};
-	}( types, clearCache, notifyDependants, set, circular );
+	}( types, clearCache, notifyDependants, notifyPatternObservers, set, circular );
 
 	/* shared/get/arrayAdaptor/patch.js */
 	var patch = function( runloop, defineProperty, getSpliceEquivalent, summariseSpliceOperation, processWrapper ) {
