59a80a0cc1bcc79e9a750a30cd138b3c3cba37e4
Rich Harris <richard.a.harris@gmail.com>
2014-06-05 00:23:01 -0400

    leftover from #788

--- src/virtualdom/items/Element/special/img/render.js
+++ src/virtualdom/items/Element/special/img/render.js
@@ -3,17 +3,17 @@ export default function renderImage ( img ) {
 
 	// if this is an <img>, and we're in a crap browser, we may need to prevent it
 	// from overriding width and height when it loads the src
-	if ( ( width = this.getAttribute( 'width' ) ) || ( height = this.getAttribute( 'height' ) ) ) {
-		this.node.addEventListener( 'load', loadHandler = function () {
+	if ( ( width = img.getAttribute( 'width' ) ) || ( height = img.getAttribute( 'height' ) ) ) {
+		img.node.addEventListener( 'load', loadHandler = function () {
 			if ( width ) {
-				this.node.width = width.value;
+				img.node.width = width.value;
 			}
 
 			if ( height ) {
-				this.node.height = height.value;
+				img.node.height = height.value;
 			}
 
-			this.node.removeEventListener( 'load', loadHandler, false );
+			img.node.removeEventListener( 'load', loadHandler, false );
 		}, false );
 	}
 }
