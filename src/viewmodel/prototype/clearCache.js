8a152175e286720d5b9c29d3e95fb284d9d9bf4c
jrajav <jrajav@gmail.com>
2014-06-11 11:00:37 -0500

    Fix jshint errors

--- src/viewmodel/prototype/clearCache.js
+++ src/viewmodel/prototype/clearCache.js
@@ -1,5 +1,5 @@
 export default function Viewmodel$clearCache ( keypath, dontTeardownWrapper ) {
-	var ractive = this.ractive, cacheMap, wrapper, computation;
+	var cacheMap, wrapper, computation;
 
 	if ( !dontTeardownWrapper ) {
 		// Is there a wrapped property at this keypath?
