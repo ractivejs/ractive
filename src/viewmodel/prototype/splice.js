12d162bd04fa7cc9556cf293a52a2d609af1382e
Rich Harris <richard.a.harris@gmail.com>
2014-06-16 14:35:32 -0400

    tidy up splice logic

--- src/viewmodel/prototype/splice.js
+++ src/viewmodel/prototype/splice.js
@@ -3,26 +3,18 @@ import types from 'config/types';
 export default function Viewmodel$splice ( keypath, spliceSummary ) {
 	var viewmodel = this, i, dependants, updateDependant, childKeypath;
 
-	// TODO surely we don't need the block before AND after
-	for ( i = spliceSummary.rangeStart; i < spliceSummary.clearEnd; i += 1 ) {
+	// Mark changed keypaths
+	for ( i = spliceSummary.rangeStart; i < spliceSummary.rangeEnd; i += 1 ) {
 		viewmodel.mark( keypath + '.' + i );
 	}
 
-	if ( dependants = viewmodel.deps[ 'default' ][ keypath ] ) {
-		dependants.filter( canSplice ).forEach( dependant => dependant.splice( spliceSummary ) );
-	}
-
-	// if we're removing old items and adding new ones, simultaneously, we need to force an update
-	if ( spliceSummary.added && spliceSummary.removed ) {
-		for ( i = spliceSummary.clearEnd; i < spliceSummary.rangeEnd; i += 1 ) {
-			childKeypath = keypath + '.' + i;
-			viewmodel.mark( childKeypath );
-		}
-	}
-
-	// If length property has changed - notify dependants
 	if ( spliceSummary.balance ) {
 		viewmodel.mark( keypath + '.length' );
+	}
+
+	// Trigger splice operations
+	if ( dependants = viewmodel.deps[ 'default' ][ keypath ] ) {
+		dependants.filter( canSplice ).forEach( dependant => dependant.splice( spliceSummary ) );
 	}
 }
 
