7c0205f0913285a0a4e51a2219e2ef11d451fa9c
Rich Harris <richard.a.harris@gmail.com>
2014-07-02 12:02:37 -0400

    kill references to fragment.nodes - generating fragments from HTML is no longer supported

--- src/virtualdom/Fragment/prototype/find.js
+++ src/virtualdom/Fragment/prototype/find.js
@@ -1,29 +1,5 @@
-import matches from 'utils/matches';
-
 export default function Fragment$find ( selector ) {
-	var i, len, item, node, queryResult;
-
-	if ( this.nodes ) {
-		len = this.nodes.length;
-		for ( i = 0; i < len; i += 1 ) {
-			node = this.nodes[i];
-
-			// we only care about elements
-			if ( node.nodeType !== 1 ) {
-				continue;
-			}
-
-			if ( matches( node, selector ) ) {
-				return node;
-			}
-
-			if ( queryResult = node.querySelector( selector ) ) {
-				return queryResult;
-			}
-		}
-
-		return null;
-	}
+	var i, len, item, queryResult;
 
 	if ( this.items ) {
 		len = this.items.length;
--- src/virtualdom/Fragment/prototype/findAll.js
+++ src/virtualdom/Fragment/prototype/findAll.js
@@ -1,32 +1,7 @@
-import matches from 'utils/matches';
-
 export default function Fragment$findAll ( selector, query ) {
-	var i, len, item, node, queryAllResult, numNodes, j;
+	var i, len, item;
 
-	if ( this.nodes ) {
-		len = this.nodes.length;
-		for ( i = 0; i < len; i += 1 ) {
-			node = this.nodes[i];
-
-			// we only care about elements
-			if ( node.nodeType !== 1 ) {
-				continue;
-			}
-
-			if ( matches( node, selector ) ) {
-				query.push( node );
-			}
-
-			if ( queryAllResult = node.querySelectorAll( selector ) ) {
-				numNodes = queryAllResult.length;
-				for ( j = 0; j < numNodes; j += 1 ) {
-					query.push( queryAllResult[j] );
-				}
-			}
-		}
-	}
-
-	else if ( this.items ) {
+	if ( this.items ) {
 		len = this.items.length;
 		for ( i = 0; i < len; i += 1 ) {
 			item = this.items[i];
