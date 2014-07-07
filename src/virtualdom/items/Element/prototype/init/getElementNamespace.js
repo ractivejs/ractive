fe7ee0665eeb689a70acd8ac308bf41a81d15061
Rich Harris <richard.a.harris@gmail.com>
2014-07-07 17:24:32 -0400

    fixes #713

--- src/virtualdom/items/Element/prototype/init/getElementNamespace.js
+++ src/virtualdom/items/Element/prototype/init/getElementNamespace.js
@@ -1,16 +1,20 @@
 import namespaces from 'config/namespaces';
 
-var defaultNamespaces = {
-	svg: namespaces.svg,
-	foreignObject: namespaces.html
-};
-
 export default function ( template, parent ) {
+	var namespace;
+
 	// if the element has an xmlns attribute, use that
 	if ( template.a && template.a.xmlns ) {
 		return template.a.xmlns;
 	}
 
-	// otherwise, guess namespace for svg/foreignObject elements, or inherit namespace from parent
-	return ( defaultNamespaces[ template.e ] || ( parent && parent.namespace ) || namespaces.html );
+	// otherwise, guess namespace for <svg/> elements, or inherit namespace from parent, unless
+	// parent is a <foreignObject/>
+	if ( template.e === 'svg' ) {
+		namespace = namespaces.svg;
+	} else if ( parent ) {
+		namespace = ( parent.name === 'foreignObject' ? namespaces.html : parent.namespace );
+	}
+
+	return namespace || namespaces.html;
 }
