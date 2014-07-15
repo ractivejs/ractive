fca7940ca3a67a2ed117c084c74f66bf0e885f9d
marty <marty.nelson@yahoo.com>
2014-07-15 14:36:02 -0700

    use common helper method in event fire

--- src/virtualdom/items/Element/EventHandler/prototype/fire.js
+++ src/virtualdom/items/Element/EventHandler/prototype/fire.js
@@ -1,5 +1,5 @@
 // This function may be overwritten, if the event directive
 // includes parameters
 export default function EventHandler$fire ( event ) {
-	this.root.fire( this.action.toString().trim(), event );
+	this.root.fire( this.getAction(), event );
 }
