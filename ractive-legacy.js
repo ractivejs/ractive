a8f928a6f3bb0f77dc201ff030b84d12d92ca42e
Travis-CI <richard.a.harris+travis@gmail.com>
2014-07-21 01:25:56 +0000

    Updated edge version

--- ractive-legacy.js
+++ ractive-legacy.js
@@ -1,6 +1,6 @@
 /*
 	ractive-legacy.js v0.5.5
-	2014-07-20 - commit 927e3440 
+	2014-07-21 - commit f650ab18 
 
 	http://ractivejs.org
 	http://twitter.com/RactiveJS
@@ -9784,16 +9784,18 @@
 
 	/* virtualdom/items/Element/special/img/render.js */
 	var render = function renderImage( img ) {
-		var width, height, loadHandler;
+		var loadHandler;
 		// if this is an <img>, and we're in a crap browser, we may need to prevent it
 		// from overriding width and height when it loads the src
-		if ( ( width = img.getAttribute( 'width' ) ) || ( height = img.getAttribute( 'height' ) ) ) {
+		if ( img.attributes.width || img.attributes.height ) {
 			img.node.addEventListener( 'load', loadHandler = function() {
-				if ( width ) {
-					img.node.width = width.value;
+				var width = img.getAttribute( 'width' ),
+					height = img.getAttribute( 'height' );
+				if ( width !== undefined ) {
+					img.node.width = width;
 				}
-				if ( height ) {
-					img.node.height = height.value;
+				if ( height !== undefined ) {
+					img.node.height = height;
 				}
 				img.node.removeEventListener( 'load', loadHandler, false );
 			}, false );
