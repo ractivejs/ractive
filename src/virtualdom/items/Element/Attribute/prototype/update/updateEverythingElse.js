1ea632d7183d1e00301362e180d3163cda220530
Martin Kolárik <martin@kolarik.me>
2014-07-23 13:32:20 +0200

    Fix #1036

--- src/virtualdom/items/Element/Attribute/prototype/update/updateEverythingElse.js
+++ src/virtualdom/items/Element/Attribute/prototype/update/updateEverythingElse.js
@@ -4,11 +4,11 @@ export default function Attribute$updateEverythingElse () {
 	var { node, namespace, name, value, fragment } = this;
 
 	if ( namespace ) {
-		node.setAttributeNS( namespace, name, fragment || value );
+		node.setAttributeNS( namespace, name, (fragment || value).toString() );
 	}
 
 	else if ( !booleanAttributes.test( name ) ) {
-		node.setAttribute( name, fragment || value );
+		node.setAttribute( name, (fragment || value).toString() );
 	}
 
 	// Boolean attributes - truthy becomes '', falsy means 'remove attribute'
