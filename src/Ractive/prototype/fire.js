dcb3f68d0572255dc09cdb228b675286cfdc2e8f
Anton Ryzhov <anton@ryzhov.me>
2014-08-07 19:50:39 +0400

    Stop event only once

--- src/Ractive/prototype/fire.js
+++ src/Ractive/prototype/fire.js
@@ -1,5 +1,5 @@
 export default function Ractive$fire ( eventName, event ) {
-	var args, i, len, result, originalEvent, subscribers = this._subs[ eventName ];
+	var args, i, len, originalEvent, stopEvent = false, subscribers = this._subs[ eventName ];
 
 	if ( !subscribers ) {
 		return;
@@ -8,10 +8,12 @@ export default function Ractive$fire ( eventName, event ) {
 	args = Array.prototype.slice.call( arguments, 1 );
 
 	for ( i=0, len=subscribers.length; i<len; i+=1 ) {
-		result = subscribers[ i ].apply( this, args );
-		if ( ( result === false ) && ( originalEvent = event.original ) ) {
-			originalEvent.preventDefault && originalEvent.preventDefault();
-			originalEvent.stopPropagation && originalEvent.stopPropagation();
+		if ( subscribers[ i ].apply( this, args ) === false ) {
+			stopEvent = true;
 		}
+	}
+	if ( stopEvent && ( originalEvent = event.original ) ) {
+		originalEvent.preventDefault && originalEvent.preventDefault();
+		originalEvent.stopPropagation && originalEvent.stopPropagation();
 	}
 }
