52306746b3aaa38486ef01968f963d8ced200b5e
Chris Reeves <chris@ev-soft.net>
2014-08-11 16:39:01 -0400

    allow components to fire events on their ancestors using the ../ notation that is used for references

--- src/Ractive/prototype/fire.js
+++ src/Ractive/prototype/fire.js
@@ -1,13 +1,37 @@
+var nameOnly = /^(?:\.?\.\/)*(.*)/;
+var parents = /^((?:\.\.\/)*).*/;
+
 export default function Ractive$fire ( eventName ) {
-	var args, i, len, subscribers = this._subs[ eventName ];
+	var args, i, len, subscribers, target, ev, upCount; 
 
-	if ( !subscribers ) {
-		return;
-	}
+	upCount = eventName.match(parents)[1].split('/').length - 1;
 
-	args = Array.prototype.slice.call( arguments, 1 );
+	if ( upCount > 0 ) {
+		target = this;
+		ev = eventName.match(nameOnly)[1];
 
-	for ( i=0, len=subscribers.length; i<len; i+=1 ) {
-		subscribers[i].apply( this, args );
+		for ( i=0; i<upCount; i+=1 ) {
+			if ( !!target ) target = target._parent;
+		}
+
+		if ( !!target && typeof target.fire === 'function' ) {
+			args = Array.prototype.slice.call(arguments, 1);
+			args[ 0 ].parent = target;
+			args[ 0 ].child = this;
+			args.unshift(ev); // make sure to pass along a non-ancestor event name
+			target.fire.apply( target, args );
+		}
+	} else {
+		subscribers = this._subs[ eventName ];
+
+		if ( !subscribers ) {
+			return;
+		}
+
+		args = Array.prototype.slice.call( arguments, 1 );
+
+		for ( i=0, len=subscribers.length; i<len; i+=1 ) {
+			subscribers[i].apply( this, args );
+		}
 	}
 }
