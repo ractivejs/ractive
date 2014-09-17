9c67b01d93e7c4cc4f4ea24017eed2a339d07965
Rich Harris <richard.a.harris@gmail.com>
2014-09-17 11:57:00 -0400

    remove a TODO

--- src/virtualdom/items/Component/prototype/unrender.js
+++ src/virtualdom/items/Component/prototype/unrender.js
@@ -1,7 +1,7 @@
 import fireEvent from 'Ractive/prototype/shared/fireEvent';
 
 export default function Component$unrender ( shouldDestroy ) {
-	fireEvent( this.instance, 'teardown', { reserved: true }); // TODO what's the meaning of reserved: true? not used anywhere AFAICT
+	fireEvent( this.instance, 'teardown' );
 
 	this.shouldDestroy = shouldDestroy;
 	this.instance.unrender();
