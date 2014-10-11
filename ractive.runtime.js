bfcb056826cf78e4905847854493de09fa22c6bd
Travis-CI <richard.a.harris+travis@gmail.com>
2014-10-11 17:22:31 +0000

    Updated edge version

--- ractive.runtime.js
+++ ractive.runtime.js
@@ -1,6 +1,6 @@
 /*
 	ractive.runtime.js v0.6.0
-	2014-10-11 - commit e02153d1 
+	2014-10-11 - commit 13580b72 
 
 	http://ractivejs.org
 	http://twitter.com/RactiveJS
@@ -1750,7 +1750,7 @@
 		}
 
 		function isHashedId( id ) {
-			return id.charAt( 0 ) === '#';
+			return id && id.charAt( 0 ) === '#';
 		}
 
 		function isParsed( template ) {
@@ -10118,7 +10118,7 @@
 				// we may be here if we have a partial like `{{>foo}}` and `foo` is the
 				// name of both a data property (whose value ISN'T the name of a partial)
 				// and a partial. In those cases, this becomes a named partial
-				if ( !template && ( template = getPartialTemplate( this.root, this.name ) ) ) {
+				if ( !template && this.name && ( template = getPartialTemplate( this.root, this.name ) ) ) {
 					unbind.call( this );
 					this.isNamed = true;
 				}
