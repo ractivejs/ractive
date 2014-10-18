27f9bc668a57cc380ff7f36b367b7d08ec8e3610
Travis-CI <richard.a.harris+travis@gmail.com>
2014-10-18 20:34:33 +0000

    Updated edge version

--- ractive-legacy.js
+++ ractive-legacy.js
@@ -1,6 +1,6 @@
 /*
 	ractive-legacy.js v0.6.0
-	2014-10-18 - commit 927c295f 
+	2014-10-18 - commit 5be14db1 
 
 	http://ractivejs.org
 	http://twitter.com/RactiveJS
@@ -2056,7 +2056,7 @@
 		};
 		arrayMemberPattern = /^\[(0|[1-9][0-9]*)\]/;
 		// if a reference is a browser global, we don't deference it later, so it needs special treatment
-		globals = /^(?:Array|Date|RegExp|decodeURIComponent|decodeURI|encodeURIComponent|encodeURI|isFinite|isNaN|parseFloat|parseInt|JSON|Math|NaN|undefined|null)$/;
+		globals = /^(?:Array|console|Date|RegExp|decodeURIComponent|decodeURI|encodeURIComponent|encodeURI|isFinite|isNaN|parseFloat|parseInt|JSON|Math|NaN|undefined|null)$/;
 		// keywords are not valid references, with the exception of `this`
 		keywords = /^(?:break|case|catch|continue|debugger|default|delete|do|else|finally|for|function|if|in|instanceof|new|return|switch|throw|try|typeof|var|void|while|with)$/;
 		return function( parser ) {
