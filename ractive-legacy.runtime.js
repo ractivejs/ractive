ea40e805b8ab46c5d93d63ffe9a85457351991a0
Travis-CI <richard.a.harris+travis@gmail.com>
2014-10-28 13:17:08 +0000

    Updated edge version

--- ractive-legacy.runtime.js
+++ ractive-legacy.runtime.js
@@ -1,6 +1,6 @@
 /*
 	ractive-legacy.runtime.js v0.6.1
-	2014-10-26 - commit 585b23ed 
+	2014-10-28 - commit 399369dc 
 
 	http://ractivejs.org
 	http://twitter.com/RactiveJS
@@ -7871,28 +7871,32 @@
 			this.attribute = element.attributes[ this.name || 'value' ];
 			interpolator = this.attribute.interpolator;
 			interpolator.twowayBinding = this;
-			if ( interpolator.keypath && interpolator.keypath.substr( 0, 2 ) === '${' ) {
-				warn( 'Two-way binding does not work with expressions (`' + interpolator.keypath.slice( 2, -1 ) + '`)' );
-				return false;
-			}
-			// A mustache may be *ambiguous*. Let's say we were given
-			// `value="{{bar}}"`. If the context was `foo`, and `foo.bar`
-			// *wasn't* `undefined`, the keypath would be `foo.bar`.
-			// Then, any user input would result in `foo.bar` being updated.
-			//
-			// If, however, `foo.bar` *was* undefined, and so was `bar`, we would be
-			// left with an unresolved partial keypath - so we are forced to make an
-			// assumption. That assumption is that the input in question should
-			// be forced to resolve to `bar`, and any user input would affect `bar`
-			// and not `foo.bar`.
-			//
-			// Did that make any sense? No? Oh. Sorry. Well the moral of the story is
-			// be explicit when using two-way data-binding about what keypath you're
-			// updating. Using it in lists is probably a recipe for confusion...
-			if ( !interpolator.keypath ) {
+			if ( keypath = interpolator.keypath ) {
+				var lastDot = keypath.lastIndexOf( '.' ),
+					test = lastDot === -1 ? keypath : keypath.substring( lastDot + 1 );
+				if ( test.substr( 0, 2 ) === '${' ) {
+					warn( 'Two-way binding does not work with expressions (`' + keypath.substr( 0, lastDot ) + test.slice( 2, -1 ) + '`)' );
+					return false;
+				}
+			} else {
+				// A mustache may be *ambiguous*. Let's say we were given
+				// `value="{{bar}}"`. If the context was `foo`, and `foo.bar`
+				// *wasn't* `undefined`, the keypath would be `foo.bar`.
+				// Then, any user input would result in `foo.bar` being updated.
+				//
+				// If, however, `foo.bar` *was* undefined, and so was `bar`, we would be
+				// left with an unresolved partial keypath - so we are forced to make an
+				// assumption. That assumption is that the input in question should
+				// be forced to resolve to `bar`, and any user input would affect `bar`
+				// and not `foo.bar`.
+				//
+				// Did that make any sense? No? Oh. Sorry. Well the moral of the story is
+				// be explicit when using two-way data-binding about what keypath you're
+				// updating. Using it in lists is probably a recipe for confusion...
 				interpolator.resolver.forceResolution();
+				keypath = interpolator.keypath;
 			}
-			this.keypath = keypath = interpolator.keypath;
+			this.keypath = keypath;
 			// initialise value, if it's undefined
 			if ( this.root.viewmodel.get( keypath ) === undefined && this.getInitialValue ) {
 				value = this.getInitialValue();
