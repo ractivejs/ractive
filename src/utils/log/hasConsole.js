78cf04208248fed85326e209ebcd61749a120c1e
marty <marty.nelson@yahoo.com>
2014-10-29 07:06:44 -0700

    jshinting

--- src/utils/log/hasConsole.js
+++ src/utils/log/hasConsole.js
@@ -1,1 +1,1 @@
-export default ( typeof console !== 'undefined' && typeof console.warn === 'function' && typeof console.warn.apply === 'function' )
+export default ( typeof console !== 'undefined' && typeof console.warn === 'function' && typeof console.warn.apply === 'function' );
