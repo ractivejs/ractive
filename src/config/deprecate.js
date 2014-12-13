a85b6ef865b9b43f7f867c625ed6a06adbb856ba
Rich Harris <richard.a.harris@gmail.com>
2014-12-13 11:40:08 -0500

    tidy up deprecate.js

--- src/config/deprecate.js
+++ src/config/deprecate.js
@@ -1,54 +1,32 @@
 import warn from 'utils/log/warn';
 import { isArray } from 'utils/is';
 
-function deprecate ( options, deprecated, correct ) {
-
-	if ( deprecated in options ) {
-
-		if( !( correct in options ) ) {
-
-			warn( getMessage( deprecated, correct ) );
-
-			options[ correct ] = options[ deprecated ];
-
-		}
-		else {
-
-			throw new Error( getMessage( deprecated, correct, true ) );
-
-		}
-
-	}
-}
-
 function getMessage( deprecated, correct, isError ) {
 	return 'options.' + deprecated + ' has been deprecated in favour of options.' + correct + '.'
 		+ ( isError ? ' You cannot specify both options, please use options.' + correct + '.' : '' );
 }
 
-
-function deprecateEventDefinitions ( options ) {
-
-	deprecate( options, 'eventDefinitions', 'events' );
+function deprecateOption ( options, deprecatedOption, correct ) {
+	if ( deprecatedOption in options ) {
+		if( !( correct in options ) ) {
+			warn( getMessage( deprecatedOption, correct ) );
+			options[ correct ] = options[ deprecatedOption ];
+		} else {
+			throw new Error( getMessage( deprecatedOption, correct, true ) );
+		}
+	}
 }
 
-function deprecateAdaptors ( options ) {
+export default function deprecate ( options ) {
+	deprecateOption( options, 'beforeInit', 'onconstruct' );
+	deprecateOption( options, 'init', 'onrender' );
+	deprecateOption( options, 'complete', 'oncomplete' );
+	deprecateOption( options, 'eventDefinitions', 'events' );
 
 	// Using extend with Component instead of options,
 	// like Human.extend( Spider ) means adaptors as a registry
 	// gets copied to options. So we have to check if actually an array
 	if ( isArray( options.adaptors ) ) {
-
-		deprecate( options, 'adaptors', 'adapt' );
-
+		deprecateOption( options, 'adaptors', 'adapt' );
 	}
-
-}
-
-export default function deprecateOptions ( options ) {
-	deprecate( options, 'beforeInit', 'onconstruct' );
-	deprecate( options, 'init', 'onrender' );
-	deprecate( options, 'complete', 'oncomplete' );
-	deprecateEventDefinitions( options );
-	deprecateAdaptors( options );
 }
