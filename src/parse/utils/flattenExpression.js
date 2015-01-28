e1074adc2dfc0fd4f9e8831898fe6adc61322fa3
Rich Harris <richard.a.harris@gmail.com>
2015-01-28 18:15:53 -0500

    simplify flattenExpression, avoid unnecessary closures

--- src/parse/utils/flattenExpression.js
+++ src/parse/utils/flattenExpression.js
@@ -8,14 +8,58 @@ export default function flattenExpression ( expression ) {
 
 	flattened = {
 		r: refs,
-		s: stringify( expression, refs )
+		s: stringify( expression )
 	};
 
-	return flattened;
-}
+	function stringify ( node ) {
+		switch ( node.t ) {
+			case BOOLEAN_LITERAL:
+			case GLOBAL:
+			case NUMBER_LITERAL:
+			return node.v;
 
-function quoteStringLiteral ( str ) {
-	return JSON.stringify( String( str ) );
+			case STRING_LITERAL:
+			return JSON.stringify( String( node.v ) );
+
+			case ARRAY_LITERAL:
+			return '[' + ( node.m ? node.m.map( stringify ).join( ',' ) : '' ) + ']';
+
+			case OBJECT_LITERAL:
+			return '{' + ( node.m ? node.m.map( stringify ).join( ',' ) : '' ) + '}';
+
+			case KEY_VALUE_PAIR:
+			return node.k + ':' + stringify( node.v );
+
+			case PREFIX_OPERATOR:
+			return ( node.s === 'typeof' ? 'typeof ' : node.s ) + stringify( node.o );
+
+			case INFIX_OPERATOR:
+			return stringify( node.o[0] ) + ( node.s.substr( 0, 2 ) === 'in' ? ' ' + node.s + ' ' : node.s ) + stringify( node.o[1] );
+
+			case INVOCATION:
+			return stringify( node.x ) + '(' + ( node.o ? node.o.map( stringify ).join( ',' ) : '' ) + ')';
+
+			case BRACKETED:
+			return '(' + stringify( node.x ) + ')';
+
+			case MEMBER:
+			return stringify( node.x ) + stringify( node.r );
+
+			case REFINEMENT:
+			return ( node.n ? '.' + node.n : '[' + stringify( node.x ) + ']' );
+
+			case CONDITIONAL:
+			return stringify( node.o[0] ) + '?' + stringify( node.o[1] ) + ':' + stringify( node.o[2] );
+
+			case REFERENCE:
+			return '_' + refs.indexOf( node.n );
+
+			default:
+			throw new Error( 'Expected legal JavaScript' );
+		}
+	}
+
+	return flattened;
 }
 
 // TODO maybe refactor this?
@@ -53,54 +97,3 @@ function extractRefs ( node, refs ) {
 	}
 }
 
-function stringify ( node, refs ) {
-	var stringifyAll = function ( item ) {
-		return stringify( item, refs );
-	};
-
-	switch ( node.t ) {
-		case BOOLEAN_LITERAL:
-		case GLOBAL:
-		case NUMBER_LITERAL:
-		return node.v;
-
-		case STRING_LITERAL:
-		return quoteStringLiteral(node.v);
-
-		case ARRAY_LITERAL:
-		return '[' + ( node.m ? node.m.map( stringifyAll ).join( ',' ) : '' ) + ']';
-
-		case OBJECT_LITERAL:
-		return '{' + ( node.m ? node.m.map( stringifyAll ).join( ',' ) : '' ) + '}';
-
-		case KEY_VALUE_PAIR:
-		return node.k + ':' + stringify( node.v, refs );
-
-		case PREFIX_OPERATOR:
-		return ( node.s === 'typeof' ? 'typeof ' : node.s ) + stringify( node.o, refs );
-
-		case INFIX_OPERATOR:
-		return stringify( node.o[0], refs ) + ( node.s.substr( 0, 2 ) === 'in' ? ' ' + node.s + ' ' : node.s ) + stringify( node.o[1], refs );
-
-		case INVOCATION:
-		return stringify( node.x, refs ) + '(' + ( node.o ? node.o.map( stringifyAll ).join( ',' ) : '' ) + ')';
-
-		case BRACKETED:
-		return '(' + stringify( node.x, refs ) + ')';
-
-		case MEMBER:
-		return stringify( node.x, refs ) + stringify( node.r, refs );
-
-		case REFINEMENT:
-		return ( node.n ? '.' + node.n : '[' + stringify( node.x, refs ) + ']' );
-
-		case CONDITIONAL:
-		return stringify( node.o[0], refs ) + '?' + stringify( node.o[1], refs ) + ':' + stringify( node.o[2], refs );
-
-		case REFERENCE:
-		return '_' + refs.indexOf( node.n );
-
-		default:
-		throw new Error( 'Expected legal JavaScript' );
-	}
-}
