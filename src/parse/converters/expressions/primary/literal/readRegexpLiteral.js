c7861d4a149375dba5a61509fcbac29ac309f8aa
Chris Reeves <chris@ev-soft.net>
2015-02-11 02:55:08 +1100

    copy/paste fail

--- src/parse/converters/expressions/primary/literal/readRegexpLiteral.js
+++ src/parse/converters/expressions/primary/literal/readRegexpLiteral.js
@@ -1,6 +1,6 @@
 import { REGEXP_LITERAL } from 'config/types';
 
-var regexpPattern = /^(\/(?:[^\n\r\u2028\u2029/\\[]|\\.|\[(?:[^\n\r\u2028\u2029\]\\]|\\.)*])+\/(?:([gimuy])(?![a-z]*\2))*(?![-a-zA-Z_$0-9]))/;
+var regexpPattern = /^(\/(?:[^\n\r\u2028\u2029/\\[]|\\.|\[(?:[^\n\r\u2028\u2029\]\\]|\\.)*])+\/(?:([gimuy])(?![a-z]*\2))*(?![a-zA-Z_$0-9]))/;
 
 export default function readNumberLiteral ( parser ) {
 	var result;
