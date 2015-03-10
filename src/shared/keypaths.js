646e1290ef0eb3df9f16f4fd5a1b62b7eb696ab0
Rich Harris <richard.a.harris@gmail.com>
2015-03-10 17:31:58 -0400

    linting

--- src/shared/keypaths.js
+++ src/shared/keypaths.js
@@ -125,7 +125,7 @@ export function getMatchingKeypaths ( ractive, keypath ) {
 	return matchingKeypaths;
 
 	function expand ( matchingKeypaths, keypath ) {
-		var wrapper, value, key, keys;
+		var wrapper, value, keys;
 
 		if ( keypath.isRoot ) {
 			keys = [].concat(
