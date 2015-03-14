fc495b71ca6427d939670bc0cbac8c091744fc6f
Rich-Harris <richard.a.harris@gmail.com>
2015-03-14 15:54:17 -0400

    allow 00 in expressions - fixes #1819

--- src/parse/converters/expressions/primary/literal/readNumberLiteral.js
+++ src/parse/converters/expressions/primary/literal/readNumberLiteral.js
@@ -1,7 +1,7 @@
 import { NUMBER_LITERAL } from 'config/types';
 
 // bulletproof number regex from https://gist.github.com/Rich-Harris/7544330
-var numberPattern = /^(?:[+-]?)(?:(?:(?:0|[1-9]\d*)?\.\d+)|(?:(?:0|[1-9]\d*)\.)|(?:0|[1-9]\d*))(?:[eE][+-]?\d+)?/;
+var numberPattern = /^(?:[+-]?)0*(?:(?:(?:[1-9]\d*)?\.\d+)|(?:(?:0|[1-9]\d*)\.)|(?:0|[1-9]\d*))(?:[eE][+-]?\d+)?/;
 
 export default function readNumberLiteral ( parser ) {
 	var result;
