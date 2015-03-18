97079f63e6f1475d3e8348c6c5bfdbcf85457f20
Travis-CI <richard.a.harris+travis@gmail.com>
2015-03-18 23:57:44 +0000

    Updated edge version

--- ractive.js
+++ ractive.js
@@ -1,6 +1,6 @@
 /*
 	Ractive.js v0.7.1
-	Wed Mar 18 2015 02:57:43 GMT+0000 (UTC) - commit 26259224a607194e6e67986e2510cee3f606427b
+	Wed Mar 18 2015 23:57:07 GMT+0000 (UTC) - commit 8947279ddbcd558bd118f241a9b834466e536b86
 
 	http://ractivejs.org
 	http://twitter.com/RactiveJS
@@ -418,7 +418,7 @@
   if (hasConsole) {
   	(function () {
   		var welcomeIntro = ["%cRactive.js %c0.7.1 %cin debug mode, %cmore...", "color: rgb(114, 157, 52); font-weight: normal;", "color: rgb(85, 85, 85); font-weight: normal;", "color: rgb(85, 85, 85); font-weight: normal;", "color: rgb(82, 140, 224); font-weight: normal; text-decoration: underline;"];
-  		var welcomeMessage = "You're running Ractive <@version@> in debug mode - messages will be printed to the console to help you fix problems and optimise your application.\n\nTo disable debug mode, add this line at the start of your app:\n  Ractive.DEBUG = false;\n\nTo disable debug mode when your app is minified, add this snippet:\n  Ractive.DEBUG = /unminified/.test(function(){/*unminified*/});\n\nGet help and support:\n  http://docs.ractivejs.org\n  http://stackoverflow.com/questions/tagged/ractivejs\n  http://groups.google.com/forum/#!forum/ractive-js\n  http://twitter.com/ractivejs\n\nFound a bug? Raise an issue:\n  https://github.com/ractivejs/ractive/issues\n\n";
+  		var welcomeMessage = "You're running Ractive 0.7.1 in debug mode - messages will be printed to the console to help you fix problems and optimise your application.\n\nTo disable debug mode, add this line at the start of your app:\n  Ractive.DEBUG = false;\n\nTo disable debug mode when your app is minified, add this snippet:\n  Ractive.DEBUG = /unminified/.test(function(){/*unminified*/});\n\nGet help and support:\n  http://docs.ractivejs.org\n  http://stackoverflow.com/questions/tagged/ractivejs\n  http://groups.google.com/forum/#!forum/ractive-js\n  http://twitter.com/ractivejs\n\nFound a bug? Raise an issue:\n  https://github.com/ractivejs/ractive/issues\n\n";
 
   		welcome = function () {
   			var hasGroup = !!console.groupCollapsed;
