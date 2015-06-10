03011b1d8a0cd7c13ed283d124b8f9680f675509
Rich Harris <richard.a.harris@gmail.com>
2015-06-10 15:22:10 -0400

    updating expressions

--- src/viewmodel/nodes/ComputedNode.js
+++ src/viewmodel/nodes/ComputedNode.js
@@ -1,9 +1,16 @@
 import { addToArray, removeFromArray } from 'utils/array';
+import { isEqual } from 'utils/is';
+import { handleChange, mark } from 'shared/methodCallers';
 
 export default class ComputedNode {
 	constructor ( viewmodel, signature ) {
 		this.viewmodel = viewmodel;
 		this.signature = signature;
+
+		this.hardDependencies = signature.dependencies;
+		this.hardDependencies.forEach( model => {
+			model.register( this );
+		});
 
 		this.value = this.getValue();
 
@@ -15,6 +22,16 @@ export default class ComputedNode {
 
 	getValue () {
 		return this.signature.getter();
+	}
+
+	handleChange () {
+		const value = this.getValue();
+		if ( isEqual( value, this.value ) ) return;
+
+		this.value = value;
+
+		this.deps.forEach( handleChange );
+		this.children.forEach( mark );
 	}
 
 	register ( dependant ) {
