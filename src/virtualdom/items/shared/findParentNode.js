63336d33523a02a89194b41ece6b4dd7196e49b3
Rich-Harris <richard.a.harris@gmail.com>
2015-06-10 21:43:03 -0400

    fix findParentNode

--- src/virtualdom/items/shared/findParentNode.js
+++ src/virtualdom/items/shared/findParentNode.js
@@ -2,9 +2,11 @@ import { ELEMENT } from 'config/types';
 
 export default function findParentNode ( item ) {
 	let fragment = item.parentFragment;
-	while ( fragment.owner.type !== ELEMENT ) {
+
+	while ( fragment ) {
+		if ( fragment.isRoot ) return fragment.ractive.el;
+		if ( fragment.owner.type === ELEMENT ) return fragment.owner.node;
+
 		fragment = fragment.parent;
 	}
-
-	return fragment.owner.node;
 }
