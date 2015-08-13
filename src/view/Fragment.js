37670bd977758374a5951a426cd2aa46d41f74e1
Chris Reeves <chris@ev-soft.net>
2015-08-13 13:32:36 -0400

    don't assume there's a parent or full set of options during fragment creation to avoid reset death

--- src/view/Fragment.js
+++ src/view/Fragment.js
@@ -22,8 +22,8 @@ export default class Fragment {
 
 		this.context = null;
 		this.rendered = false;
-		this.indexRefs = options.indexRefs || this.parent.indexRefs;
-		this.keyRefs = options.keyRefs || this.parent.keyRefs;
+		this.indexRefs = options.indexRefs || ( this.parent ? this.parent.indexRefs : [] );
+		this.keyRefs = options.keyRefs || ( this.parent ? this.parent.keyRefs : {} );
 
 		// encapsulated styles should be inherited until they get applied by an element
 		this.cssIds = 'cssIds' in options ? options.cssIds : ( this.parent ? this.parent.cssIds : null );
