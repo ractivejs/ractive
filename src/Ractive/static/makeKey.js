8655e2f4eee7248ded1276e167abada98643cf50
Nicholas Arthur <narthur@adobe.com>
2015-08-17 16:39:12 -0400

    makeKey: Make use of ES6 and fix whitespace

--- src/Ractive/static/makeKey.js
+++ src/Ractive/static/makeKey.js
@@ -2,16 +2,6 @@
  * @param {Array} argument - series of keys
  * @return {string} keypath
  */
-export default function Ractive$makeKey() {
-	var keypath = "";
-
-	for ( var i = 0; i < arguments.length; i++ ) {
-		keypath += arguments[i];
-
-		if ( i !== arguments.length - 1 ) {
-			keypath += ".";
-		}
-	}
-
-	return keypath;
+export default function Ractive$makeKey(...keys) {
+	return keys.join('.');
 }
--- test/__tests/makeKey.js
+++ test/__tests/makeKey.js
@@ -4,7 +4,7 @@ module( 'makeKey()' );
 
 test( 'Returns keypath from a series of keys', t => {
 	var keys = ['a', 'b', 'c', 'd'],
-			expected = 'a.b.c.d';
+		expected = 'a.b.c.d';
 
 	t.equal(Ractive.makeKey.apply(this, keys), expected);
 });
