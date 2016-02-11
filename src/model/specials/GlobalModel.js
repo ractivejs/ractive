44fe41fcd7332081406c55b143142e12f36e3689
Serhan Apaydın <zerhan@gmail.com>
2016-02-11 12:00:55 +0200

    Fix server-side "window is not defined" error
    Fixes "window is not defined" error when Ractive.js is used in a server-side library (i.e. ClearScript)

--- src/model/specials/GlobalModel.js
+++ src/model/specials/GlobalModel.js
@@ -4,7 +4,7 @@ import Model from '../Model';
 class GlobalModel extends Model {
 	constructor ( ) {
 		super( null, '@global' );
-		this.value = typeof global !== 'undefined' ? global : window;
+		this.value = typeof global !== 'undefined' ? global : win;
 		this.isRoot = true;
 		this.root = this;
 		this.adaptors = [];
