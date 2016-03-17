b67c8591e839d93dc51dbfda2a9248044f948458
Chris Reeves <chris@ev-soft.net>
2016-03-16 21:12:08 -0400

    allow a query selector to be passed to getNodeInfo if it's supported by the env

--- src/Ractive/static/getNodeInfo.js
+++ src/Ractive/static/getNodeInfo.js
@@ -1,7 +1,13 @@
 import gatherRefs from '../../view/helpers/gatherRefs';
 import { addHelpers } from '../../view/helpers/contextMethods';
 
+const query = document && document.querySelector;
+
 export default function( node ) {
+	if ( typeof node === 'string' && query ) {
+		node = query.call( document, node );
+	}
+
 	if ( !node || !node._ractive ) return {};
 
 	const storage = node._ractive;
