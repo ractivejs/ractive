0c4e24be4784ee113f126c4ac93860d257b67b77
Chris Reeves <chris@ev-soft.net>
2016-03-19 01:22:34 -0400

    add basic tests for non-destructive style and class attribute handling

---
+++ test/browser-tests/attributes.js
@@ -0,0 +1,31 @@
+import { test } from 'qunit';
+
+test( `class attributes only update the classes in their content`, t => {
+	const r = new Ractive({
+		el: fixture,
+		template: `<span class="{{classes}}" />`,
+		data: { classes: 'foo bar' }
+	});
+	const span = r.find( 'span' );
+
+	span.className += ' yep';
+	r.set( 'classes', 'foo baz' );
+
+	t.equal( span.className, 'foo yep baz' );
+});
+
+test( `style attributes only update the styles in their content`, t => {
+	const r = new Ractive({
+		el: fixture,
+		template: `<span style="{{styles}}" />`,
+		data: { styles: 'width: 100px; height: 99px' }
+	});
+	const span = r.find( 'span' );
+
+	span.style.display = 'block';
+	r.set( 'styles', 'color: red; height: 87.312%;' );
+
+	t.equal( span.style.display, 'block' );
+	t.equal( span.style.color, 'red' );
+	t.equal( span.style.height, '87.312%' );
+});
