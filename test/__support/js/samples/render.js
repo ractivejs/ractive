0997874bc5ea1c341ae3502954872c6b087d2c45
Chris Reeves <chris@ev-soft.net>
2016-03-21 16:25:48 -0400

    test for basic keypath expression

--- test/__support/js/samples/render.js
+++ test/__support/js/samples/render.js
@@ -1042,6 +1042,12 @@ const renderTests = [
 		result: `<ul><li>items.0.some - a</li><li>items.2.some - c</li></ul>`
 	},
 	{
+		name: '@keypath(...) can be used to refer to a path relative to the current context',
+		template: `<ul>{{#items}}{{#some}}<li>{{@keypath(../)}} - {{path}}</li>{{/}}{{/}}</ul>`,
+		data: { items: [ { some: { path: 'a' } }, { notsome: { path: 'b' } }, { some: { path: 'c' } } ] },
+		result: `<ul><li>items.0 - a</li><li>items.2 - c</li></ul>`
+	},
+	{
 		name: '@key references can be strings that look like numbers',
 		template: '{{#each object:k}}{{@key}} {{k}}{{/each}}',
 		data: { object: { '0001': 1 } },
