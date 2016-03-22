ed900bee6e63f3f0c9e3b3c912a95cdbb15ec680
Chris Reeves <chris@ev-soft.net>
2016-03-22 00:34:20 -0400

    regex-based css parser because @MartinKolarik was here

--- src/view/helpers/specialAttrs.js
+++ src/view/helpers/specialAttrs.js
@@ -1,73 +1,25 @@
+import camelCase from '../../utils/camelCase';
+
 const space = /\s+/;
-const ident = /[a-zA-Z]/;
-const trim = /^\s*(.*)\s*$/;
 const specials = { 'float': 'cssFloat' };
+const remove = /\/\*(?:[\s\S]*?)\*\//g;
+const escape = /url\(\s*(['"])(?:\\[\s\S]|(?!\1).)*\1\s*\)|url\((?:\\[\s\S]|[^)])*\)|(['"])(?:\\[\s\S]|(?!\1).)*\2/gi;
+const value = /\0(\d+)/g;
 
 export function readStyle ( css ) {
-  let attrs = {};
-  let c, i = 0, txt = '', quote, id = true, last, cap = false, comment = false;
-  for ( i = 0; i < css.length; i++ ) {
-    c = css[i];
+    let values = [];
 
-    // inside a comment
-    if ( comment ) {
-      if ( c === '*' && css[i + 1] === '/' ) {
-        comment = false;
-        i++;
-        continue;
-      } else continue;
-    }
-
-    // inside a quote
-    if ( quote ) {
-      txt += c;
-
-      // escapes
-      if ( c === '\\' ) {
-        txt += css[++i];
-        continue;
-      }
-
-      // end quote
-      if ( c === quote ) {
-        quote = false;
-      }
-
-      continue;
-    }
-
-    if ( space.test( c ) ) {
-      if ( !id ) txt += c;
-    } else if ( id && ident.test( c ) ) {
-      if ( id === true ) id = c;
-      else id += cap ? c.toUpperCase() : c;
-    } else if ( id && c === '-' && id !== true ) {
-      cap = true;
-      continue;
-    } else if ( c === '"' || c === "'" ) {
-      txt += c;
-      quote = c;
-    } else if ( c === ':' ) {
-      last = specials[id] || id;
-      id = false;
-    } else if ( c === ';' ) {
-      if ( last ) attrs[ last ] = txt.replace( trim, '$1' );
-      txt = '';
-      id = true;
-      last = false;
-    } else if ( c === '/' && css[i + 1] === '*' ) {
-      comment = true;
-      i++;
-    } else if ( !id ) {
-      txt += c;
-    }
-
-    cap = false;
-  }
-
-  if ( last ) attrs[ last ] = txt.replace( trim, '$1' );
-
-  return attrs;
+    return css.replace( escape, match => `\0${values.push( match ) - 1}`)
+        .replace( remove, '' )
+        .split( ';' )
+        .filter( rule => !!rule.trim() )
+        .map( rule => rule.replace( value, ( match, n ) => values[ n ] ) )
+        .reduce(( rules, rule ) => {
+            let i = rule.indexOf(':');
+            let name = camelCase( rule.substr( 0, i ).trim() );
+            rules[ specials[ name ] || name ] = rule.substr( i + 1 ).trim();
+            return rules;
+        }, {});
 }
 
 export function readClass ( str ) {
