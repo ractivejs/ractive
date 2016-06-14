b57cf5d5f427c4f213448efec48d3bbc53d983eb
Travis-CI <richard.a.harris+travis@gmail.com>
2016-06-14 23:26:01 +0000

    Updated edge version

--- ractive.js
+++ ractive.js
@@ -1,6 +1,6 @@
 /*
 	Ractive.js v0.8.0-edge
-	Tue Jun 14 2016 19:59:29 GMT+0000 (UTC) - commit af1ecaa35cbd61305b887036ae8de3907e617e96
+	Tue Jun 14 2016 23:27:48 GMT+0000 (UTC) - commit 7b875f25f8e4e832c8f89fb853e7f8b777ebaf8b
 
 	http://ractivejs.org
 	http://twitter.com/RactiveJS
@@ -432,13 +432,13 @@
   var welcome;
   if ( hasConsole ) {
   	var welcomeIntro = [
-  		("%cRactive.js %c0.8.0-edge-af1ecaa35cbd61305b887036ae8de3907e617e96 %cin debug mode, %cmore..."),
+  		("%cRactive.js %c0.8.0-edge-7b875f25f8e4e832c8f89fb853e7f8b777ebaf8b %cin debug mode, %cmore..."),
   		'color: rgb(114, 157, 52); font-weight: normal;',
   		'color: rgb(85, 85, 85); font-weight: normal;',
   		'color: rgb(85, 85, 85); font-weight: normal;',
   		'color: rgb(82, 140, 224); font-weight: normal; text-decoration: underline;'
   	];
-  	var welcomeMessage = "You're running Ractive 0.8.0-edge-af1ecaa35cbd61305b887036ae8de3907e617e96 in debug mode - messages will be printed to the console to help you fix problems and optimise your application.\n\nTo disable debug mode, add this line at the start of your app:\n  Ractive.DEBUG = false;\n\nTo disable debug mode when your app is minified, add this snippet:\n  Ractive.DEBUG = /unminified/.test(function(){/*unminified*/});\n\nGet help and support:\n  http://docs.ractivejs.org\n  http://stackoverflow.com/questions/tagged/ractivejs\n  http://groups.google.com/forum/#!forum/ractive-js\n  http://twitter.com/ractivejs\n\nFound a bug? Raise an issue:\n  https://github.com/ractivejs/ractive/issues\n\n";
+  	var welcomeMessage = "You're running Ractive 0.8.0-edge-7b875f25f8e4e832c8f89fb853e7f8b777ebaf8b in debug mode - messages will be printed to the console to help you fix problems and optimise your application.\n\nTo disable debug mode, add this line at the start of your app:\n  Ractive.DEBUG = false;\n\nTo disable debug mode when your app is minified, add this snippet:\n  Ractive.DEBUG = /unminified/.test(function(){/*unminified*/});\n\nGet help and support:\n  http://docs.ractivejs.org\n  http://stackoverflow.com/questions/tagged/ractivejs\n  http://groups.google.com/forum/#!forum/ractive-js\n  http://twitter.com/ractivejs\n\nFound a bug? Raise an issue:\n  https://github.com/ractivejs/ractive/issues\n\n";
 
   	welcome = function () {
   		var hasGroup = !!console.groupCollapsed;
@@ -10175,6 +10175,13 @@
   		return remapped;
   	};
 
+  	RootModel.prototype.mark = function mark$1 () {
+  		var this$1 = this;
+
+  		Object.keys( this.mappings ).forEach( function ( k ) { return this$1.mappings[k].mark(); } );
+  		Model.prototype.mark.call(this);
+  	};
+
   	RootModel.prototype.resetMappings = function resetMappings () {
   		for ( var k in this.mappings ) {
   			this.mappings[k].unregister( this );
@@ -16245,7 +16252,7 @@
   	magic:          { value: magicSupported },
 
   	// version
-  	VERSION:        { value: '0.8.0-edge-af1ecaa35cbd61305b887036ae8de3907e617e96' },
+  	VERSION:        { value: '0.8.0-edge-7b875f25f8e4e832c8f89fb853e7f8b777ebaf8b' },
 
   	// plugins
   	adaptors:       { writable: true, value: {} },
