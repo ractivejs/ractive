2fb613294e9316e8d29f96b2688760c16cb74138
Travis-CI <richard.a.harris+travis@gmail.com>
2016-09-19 20:55:06 +0000

    Updated edge version

--- ractive-legacy.runtime.js
+++ ractive-legacy.runtime.js
@@ -1,6 +1,6 @@
 /*
 	Ractive.js v0.8.0-edge
-	Mon Sep 19 2016 19:57:17 GMT+0000 (UTC) - commit a3390b83446e64abaecdf1282b382cbdc96495e0
+	Mon Sep 19 2016 20:56:35 GMT+0000 (UTC) - commit 7d61ca46cd19f3aff9d020897018fc25c0e0a13b
 
 	http://ractivejs.org
 	http://twitter.com/RactiveJS
@@ -911,13 +911,13 @@
   var welcome;
   if ( hasConsole ) {
   	var welcomeIntro = [
-  		("%cRactive.js %c0.8.0-edge-a3390b83446e64abaecdf1282b382cbdc96495e0 %cin debug mode, %cmore..."),
+  		("%cRactive.js %c0.8.0-edge-7d61ca46cd19f3aff9d020897018fc25c0e0a13b %cin debug mode, %cmore..."),
   		'color: rgb(114, 157, 52); font-weight: normal;',
   		'color: rgb(85, 85, 85); font-weight: normal;',
   		'color: rgb(85, 85, 85); font-weight: normal;',
   		'color: rgb(82, 140, 224); font-weight: normal; text-decoration: underline;'
   	];
-  	var welcomeMessage = "You're running Ractive 0.8.0-edge-a3390b83446e64abaecdf1282b382cbdc96495e0 in debug mode - messages will be printed to the console to help you fix problems and optimise your application.\n\nTo disable debug mode, add this line at the start of your app:\n  Ractive.DEBUG = false;\n\nTo disable debug mode when your app is minified, add this snippet:\n  Ractive.DEBUG = /unminified/.test(function(){/*unminified*/});\n\nGet help and support:\n  http://docs.ractivejs.org\n  http://stackoverflow.com/questions/tagged/ractivejs\n  http://groups.google.com/forum/#!forum/ractive-js\n  http://twitter.com/ractivejs\n\nFound a bug? Raise an issue:\n  https://github.com/ractivejs/ractive/issues\n\n";
+  	var welcomeMessage = "You're running Ractive 0.8.0-edge-7d61ca46cd19f3aff9d020897018fc25c0e0a13b in debug mode - messages will be printed to the console to help you fix problems and optimise your application.\n\nTo disable debug mode, add this line at the start of your app:\n  Ractive.DEBUG = false;\n\nTo disable debug mode when your app is minified, add this snippet:\n  Ractive.DEBUG = /unminified/.test(function(){/*unminified*/});\n\nGet help and support:\n  http://docs.ractivejs.org\n  http://stackoverflow.com/questions/tagged/ractivejs\n  http://groups.google.com/forum/#!forum/ractive-js\n  http://twitter.com/ractivejs\n\nFound a bug? Raise an issue:\n  https://github.com/ractivejs/ractive/issues\n\n";
 
   	welcome = function () {
   		var hasGroup = !!console.groupCollapsed;
@@ -12055,6 +12055,7 @@
   		this.context = next;
   	this.iterations.forEach( function ( fragment ) {
   		var model = next ? next.joinKey( fragment.key || fragment.index ) : undefined;
+  		fragment.context = model;
   		if ( this$1.owner.template.z ) {
   			fragment.aliases = {};
   			fragment.aliases[ this$1.owner.template.z[0].n ] = model;
@@ -14708,7 +14709,7 @@
   	magic:          { value: magicSupported },
 
   	// version
-  	VERSION:        { value: '0.8.0-edge-a3390b83446e64abaecdf1282b382cbdc96495e0' },
+  	VERSION:        { value: '0.8.0-edge-7d61ca46cd19f3aff9d020897018fc25c0e0a13b' },
 
   	// plugins
   	adaptors:       { writable: true, value: {} },
