a627828f894111e6b2d0481ec3a3a925f8e276bb
Chris Reeves <evschris@gmail.com>
2016-11-04 00:25:12 -0400

    try to normalize npm tagging a bit

--- scripts/edge-release.sh
+++ scripts/edge-release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 VERSION=$(cat package.json | grep "version" | sed 's/"version": "\(.*\)",/\1/' | sed 's/[[:space:]]//g')
 VERSION_NUM=$(echo $VERSION | sed 's/[^0-9\./\]//g')
+TAG="v$(cat package.json | grep "version" | sed 's/"version": "\([0-9]*\.[0-9]\).*",/\1/' | sed 's/[[:space:]]//g')-dev"
 
 # find the last published build number for this series
 LAST=$(npm show ractive versions | grep "${VERSION_NUM}-build-" | tail -n 1 | grep "${VERSION_NUM}-build-")
@@ -13,8 +14,9 @@ LAST=$((LAST + 1))
 
 TARGET="${VERSION_NUM}-build-${LAST}"
 
-if [ -z $TAG ]; then
-	TAG='edge'
+REV=$(git rev-parse --abbrev-ref HEAD)
+if [ "$REV" = "dev" ]; then
+	EDGE_TAG="edge"
 fi
 
 echo
@@ -38,6 +40,11 @@ echo '> publishing to npm...'
 	node -e "var package = JSON.parse(fs.readFileSync('./package.json')); package.version = '${TARGET}'; fs.writeFileSync('./package.json', JSON.stringify(package, null, '  '));"
 	# ...and to npm
 	npm publish --tag $TAG
+
+	if [ ! -z $EDGE_TAG ]; then
+		echo "also publishing as $EDGE_TAG"
+		npm dist-tag add ractive@$TARGET $EDGE_TAG
+	fi
 )
 
 echo '> release complete'
