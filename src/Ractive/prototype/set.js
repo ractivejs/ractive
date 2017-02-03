034b60f49fc2444244207f6f1df8508836e9bb43
Chris Reeves <chris@ev-soft.net>
2017-02-02 23:54:27 -0500

    account for set with an object and options

--- src/Ractive/prototype/set.js
+++ src/Ractive/prototype/set.js
@@ -2,7 +2,8 @@ import { build, set } from '../../shared/set';
 
 export default function Ractive$set ( keypath, value, options ) {
 	const ractive = this;
+	const opts = typeof keypath === 'object' ? value : options;
 
-	return set( ractive, build( ractive, keypath, value ), options );
+	return set( ractive, build( ractive, keypath, value ), opts );
 }
 
