0413e6fe8d53351ab3bdc44f5d5d6353584ee6ae
Ash Kyd <ash@kyd.com.au>
2017-02-12 01:06:34 +1000

    fix #2871 infinite loop on non-ractive el

--- src/view/items/shared/EventDirective.js
+++ src/view/items/shared/EventDirective.js
@@ -29,20 +29,19 @@ export const DelegateProxy = {
 			// starting with the origin node, walk up the DOM looking for ractive nodes with a matching event listener
 			while ( bubble && node !== end ) {
 				const el = node._ractive && node._ractive.proxy;
-				if ( el ) {
+				if ( !el ) break;
 
-					// set up the context for the handler
-					event.node = el.node;
-					event.name = name;
+				// set up the context for the handler
+				event.node = el.node;
+				event.name = name;
 
-					el.events.forEach( ev => {
-						if ( ~ev.template.n.indexOf( name ) ) {
-							bubble = ev.fire( event, passedArgs ) !== false && bubble;
-						}
-					});
+				el.events.forEach( ev => {
+					if ( ~ev.template.n.indexOf( name ) ) {
+						bubble = ev.fire( event, passedArgs ) !== false && bubble;
+					}
+				});
 
-					node = node.parentNode;
-				}
+				node = node.parentNode;
 			}
 
 			return bubble;
