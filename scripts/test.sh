d43d8426c5a12425a4146676a48f69b62a3ca8f5
Chris Reeves <chris@ev-soft.net>
2017-02-11 19:28:32 -0500

    rename ractive.runtime.js to runtime.js because - fixes #1709

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,7 +10,7 @@ echo "> running node.js-specific tests. working directory is $PWD"
 qunit-cli --quiet --code Ractive:./tmp/ractive.js ./tmp/test/node-tests/index.js
 
 # check ractive.runtime doesn't error (#1860)
-node tmp/ractive.runtime.js
+node tmp/runtime.js
 
 # run browser tests
 $MOD/phantomjs scripts/phantom-runner.js
