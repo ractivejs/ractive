3ac692dd3bdfbad5153c3b75f08bd06bc77c7a06
Chris Reeves <chris@ev-soft.net>
2017-02-17 12:51:16 -0500

    remove magic and array adaptors

--- src/config/environment.js
+++ src/config/environment.js
@@ -4,18 +4,10 @@ const doc = win ? document : null;
 const isClient = !!doc;
 const hasConsole = ( typeof console !== 'undefined' && typeof console.warn === 'function' && typeof console.warn.apply === 'function' );
 
-let magic;
-try {
-	Object.defineProperty({}, 'test', { value: 0 });
-	magic = true;
-} catch ( e ) {
-	magic = false;
-}
-
 const svg = doc ?
 	doc.implementation.hasFeature( 'http://www.w3.org/TR/SVG11/feature#BasicStructure', '1.1' ) :
 	false;
 
 const vendors = [ 'o', 'ms', 'moz', 'webkit' ];
 
-export { win, doc, isClient, hasConsole, magic, svg, vendors };
+export { win, doc, isClient, hasConsole, svg, vendors };
--- test/browser-tests/render/output.js
+++ test/browser-tests/render/output.js
@@ -14,62 +14,59 @@ export default function() {
 		if ( !svg && theTest.svg ) return;
 		if ( theTest.nodeOnly ) return;
 
-		[ false, true ].forEach( magic => {
-			test( `${theTest.name} (magic: ${magic})`, t => {
+		test( theTest.name, t => {
 
-				// suppress warnings about non-POJOs and failed computations
-				onWarn( msg => t.ok( /plain JavaScript object|Failed to compute/.test( msg ) ) );
+			// suppress warnings about non-POJOs and failed computations
+			onWarn( msg => t.ok( /plain JavaScript object|Failed to compute/.test( msg ) ) );
 
-				// we don't render to an element initially, so we can see what would happen if this was happening server-side with no DOM
-				const ractive = new Ractive({
-					data: getData( theTest.data || {} ),
-					template: theTest.template,
-					partials: theTest.partials,
-					debug: true,
-					magic
-				});
-
-
-				t.htmlEqual( ractive.toHTML(), theTest.result, 'initial toHTML() should match result' );
-
-				if ( theTest.new_data ) {
-					ractive.set( theTest.new_data );
-
-					t.htmlEqual( ractive.toHTML(), theTest.new_result, 'new toHTML() should match result' );
-				} else if ( theTest.steps && theTest.steps.length ) {
-					theTest.steps.forEach( ( step, i ) => {
-						ractive.set( getData( step.data || {} ) );
-
-						t.htmlEqual( ractive.toHTML(), step.result, `step ${i}: ` + ( step.message || 'toHTML() should match result' ) );
-					});
-				}
-
-				// some tests expect certain orderings that only happen when templates are unbound
-				// so we reset to empty and then reload the data before rendering
-				ractive.reset();
-				ractive.reset( getData( theTest.data ) );
-				ractive.render( fixture );
-
-				// we'll also check toHTML again, to make sure it still behaves as expected when rendered
-				t.htmlEqual( fixture.innerHTML, theTest.result, 'initial innerHTML should match result' );
-				t.htmlEqual( ractive.toHTML(), theTest.result, 'initial toHTML() should match result' );
-
-				if ( theTest.new_data ) {
-					ractive.set( theTest.new_data );
-
-					t.htmlEqual( fixture.innerHTML, theTest.new_result, 'new innerHTML should match result' );
-					t.htmlEqual( ractive.toHTML(), theTest.new_result, 'new toHTML() should match result' );
-				} else if ( theTest.steps && theTest.steps.length ) {
-					theTest.steps.forEach( ( step, i ) => {
-						ractive.set( getData( step.data || {} ) );
-
-						t.htmlEqual( fixture.innerHTML, step.result, `step ${i}: ` + ( step.message || 'innerHTML should match result' ) );
-						t.htmlEqual( ractive.toHTML(), step.result, `step ${i}: ` + ( step.message || 'toHTML() should match result' ) );
-					});
-				}
-
-				ractive.teardown();
+			// we don't render to an element initially, so we can see what would happen if this was happening server-side with no DOM
+			const ractive = new Ractive({
+				data: getData( theTest.data || {} ),
+				template: theTest.template,
+				partials: theTest.partials,
+				debug: true
 			});
+
+
+			t.htmlEqual( ractive.toHTML(), theTest.result, 'initial toHTML() should match result' );
+
+			if ( theTest.new_data ) {
+				ractive.set( theTest.new_data );
+
+				t.htmlEqual( ractive.toHTML(), theTest.new_result, 'new toHTML() should match result' );
+			} else if ( theTest.steps && theTest.steps.length ) {
+				theTest.steps.forEach( ( step, i ) => {
+					ractive.set( getData( step.data || {} ) );
+
+					t.htmlEqual( ractive.toHTML(), step.result, `step ${i}: ` + ( step.message || 'toHTML() should match result' ) );
+				});
+			}
+
+			// some tests expect certain orderings that only happen when templates are unbound
+			// so we reset to empty and then reload the data before rendering
+			ractive.reset();
+			ractive.reset( getData( theTest.data ) );
+			ractive.render( fixture );
+
+			// we'll also check toHTML again, to make sure it still behaves as expected when rendered
+			t.htmlEqual( fixture.innerHTML, theTest.result, 'initial innerHTML should match result' );
+			t.htmlEqual( ractive.toHTML(), theTest.result, 'initial toHTML() should match result' );
+
+			if ( theTest.new_data ) {
+				ractive.set( theTest.new_data );
+
+				t.htmlEqual( fixture.innerHTML, theTest.new_result, 'new innerHTML should match result' );
+				t.htmlEqual( ractive.toHTML(), theTest.new_result, 'new toHTML() should match result' );
+			} else if ( theTest.steps && theTest.steps.length ) {
+				theTest.steps.forEach( ( step, i ) => {
+					ractive.set( getData( step.data || {} ) );
+
+					t.htmlEqual( fixture.innerHTML, step.result, `step ${i}: ` + ( step.message || 'innerHTML should match result' ) );
+					t.htmlEqual( ractive.toHTML(), step.result, `step ${i}: ` + ( step.message || 'toHTML() should match result' ) );
+				});
+			}
+
+			ractive.teardown();
 		});
 	});
 
