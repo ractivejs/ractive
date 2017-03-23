766d87885c471cfdf15424f3f37b8eff03a17811
Jeff Wear <jeff@mixmax.com>
2017-03-22 20:30:39 -0700

    Ensure we bind functions using `Function.prototype.bind`
    Whereas the function itself may have overridden `bind`, for instance if it
    is Underscore: https://github.com/jashkenas/underscore/blob/8fc7032295d60aff3620ef85d4aa6549a55688a0/underscore.js#L765
    
    Fixes #2915.
    
    A similar fix was put in place in 6ce7082f67c963ec078c4e7b6f561c2cbbd5300c
    to fix #1055, but that was lost somewhere along the way. This defeated the
    regression test for #1055 because `src/utils/bind.js` only called `bind` on
    _functions_ that _used `this`_ and the regression test for #1055 simulated
    Underscore using a POJO.

--- src/utils/bind.js
+++ src/utils/bind.js
@@ -1,7 +1,9 @@
+const fnBind = Function.prototype.bind;
+
 export default function bind ( fn, context ) {
 	if ( !/this/.test( fn.toString() ) ) return fn;
 
-	const bound = fn.bind( context );
+	const bound = fnBind.call( fn, context );
 	for ( const prop in fn ) bound[ prop ] = fn[ prop ];
 
 	return bound;
