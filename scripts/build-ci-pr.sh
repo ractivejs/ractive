169ea9a67d9f5f09ade32afe9d10481cc7705625
Chris Reeves <chris@ev-soft.net>
2017-06-02 16:31:05 -0400

    attempting to brute-force sourcemapped coverage for coveralls

--- scripts/build-ci-pr.sh
+++ scripts/build-ci-pr.sh
@@ -10,9 +10,11 @@ npm run lint:tests:browser
 npm run lint:tests:node
 
 npm run bundle:test
+cp -r .gobble-build/02-replacePlaceholders/.cache .build/src
+node scripts/swizzle-coverage.js .build/ractive.js
 npm run test:phantom
 npm run test:electron
-npm run test:saucelabs || true # Allow to fail for now
+#npm run test:saucelabs || true # Allow to fail for now
 npm run test:coverage
 
 npm run report:coveralls
