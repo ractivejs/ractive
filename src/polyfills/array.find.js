973f71a0fad42b13d99d6d8b02afb9d15da2e5f4
Chris Reeves <chris@ev-soft.net>
2017-07-10 17:57:11 -0400

    use defineproperty when polyfilling array.find - #3038

--- src/polyfills/array.find.js
+++ src/polyfills/array.find.js
@@ -1,20 +1,24 @@
 if (!Array.prototype.find) {
-	Array.prototype.find = function (callback, thisArg) {
-		if (this === null || this === undefined)
-			throw new TypeError('Array.prototype.find called on null or undefined');
+	Object.defineProperty( Array.prototype, 'find', {
+		value (callback, thisArg) {
+			if (this === null || this === undefined)
+				throw new TypeError('Array.prototype.find called on null or undefined');
 
-		if (typeof callback !== 'function')
-			throw new TypeError(`${callback} is not a function`);
+			if (typeof callback !== 'function')
+				throw new TypeError(`${callback} is not a function`);
 
-		const array = Object(this);
-		const arrayLength = array.length >>> 0;
+			const array = Object(this);
+			const arrayLength = array.length >>> 0;
 
-		for (let index = 0; index < arrayLength; index++) {
-			if (!Object.hasOwnProperty.call(array, index)) continue;
-			if (!callback.call(thisArg, array[index], index, array)) continue;
-			return array[index];
-		}
+			for (let index = 0; index < arrayLength; index++) {
+				if (!Object.hasOwnProperty.call(array, index)) continue;
+				if (!callback.call(thisArg, array[index], index, array)) continue;
+				return array[index];
+			}
 
-		return undefined;
-	};
+			return undefined;
+		},
+		configurable: true,
+		writable: true
+	});
 }
