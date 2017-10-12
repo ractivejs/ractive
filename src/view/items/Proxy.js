a77d7bd9e8ceaa06cd147f1596b8f9b813800115
Chris Reeves <chris@ev-soft.net>
2017-10-12 14:48:55 -0400

    add support for claimed attributes on proxies including excluding them from the main template, gathering them to be tracked for the intermediary, providing an extra-attributes partial, and firing them on the inital intermediary call and optionally on each update

--- src/view/items/Proxy.js
+++ src/view/items/Proxy.js
@@ -7,30 +7,57 @@ import runloop from 'src/global/runloop';
 import { PROXY } from 'config/types';
 import { applyCSS } from 'src/global/css';
 import { isArray } from 'utils/is';
-import { assign, create, hasOwn } from 'utils/object';
+import { assign, create, hasOwn, keys } from 'utils/object';
+import noop from 'utils/noop';
 
 // thingy that can supply css and template
 export default function Proxy ( options, fn ) {
 	ContainerItem.call( this, options );
 
-	const template = this.template;
+	// make a defensive shallow copy of the template
+	const template = this.template = assign( {}, this.template );
 
 	this.fn = fn;
 	this.type = PROXY;
 	this.name = template.e;
 
 	const handle = this.handle = this.up.getContext();
-	handle.template = options.template;
+	handle.template = template;
 	handle.proxy = this;
 	handle.refresh = refresh;
 	handle.name = template.e;
+	handle.attrs = {};
+	this.dirtyAttrs = false;
 
 	if ( !template.p ) template.p = {};
+	handle.partials = assign( {}, template.p );
 	if ( !hasOwn( template.p, 'content' ) ) template.p.content = template.f || [];
 
-	this.proxy = fn( handle );
+	if ( isArray( fn.attributes ) ) {
+		this._attrs = {};
 
-	this.redo();
+		const me = this;
+		const invalidate = function () {
+			this.dirty = true;
+			me.dirtyAttrs = true;
+			me.bubble();
+		};
+
+		if ( isArray( template.m ) ) {
+			const attrs = template.m;
+			template.m = attrs.filter( a => !~fn.attributes.indexOf( a.n ) );
+			template.p['extra-attributes'] = template.m;
+			attrs.filter( a => ~fn.attributes.indexOf( a.n ) ).forEach( a => {
+				const fragment = new Fragment({
+					template: a.f,
+					owner: this
+				});
+				fragment.bubble = invalidate;
+				fragment.findFirstNode = noop;
+				this._attrs[ a.n ] = fragment;
+			});
+		}
+	}
 }
 
 const proto = Proxy.prototype = create( ContainerItem.prototype );
@@ -39,16 +66,32 @@ assign( proto, {
 	constructor: Proxy,
 
 	bind () {
-		this.bound = true;
-		if ( this.fragment ) this.fragment.bind();
+		if ( !this.bound ) {
+			this.bound = true;
+
+			// bind attributes
+			if ( this._attrs ) {
+				keys( this._attrs ).forEach( k => {
+					this._attrs[k].bind();
+				});
+				this.refreshAttrs();
+			}
+
+			this.proxy = this.fn( this.handle, this.handle.attrs );
+
+			this.redo();
+
+			if ( this.fragment ) this.fragment.bind();
+		}
 	},
 
 	redo () {
-		const wasBound = this.bound;
-		const wasRendered = this.rendered;
+		let fragment = this.fragment;
+		const wasBound = fragment && fragment.bound;
+		const wasRendered = fragment && fragment.rendered;
 
-		this.unbind();
-		this.unrender( true );
+		if ( wasBound ) fragment.unbind();
+		if ( wasRendered ) fragment.unrender( true );
 
 		let tpl = this.proxy.template;
 		if ( !isArray( tpl ) ) {
@@ -69,14 +112,21 @@ assign( proto, {
 			}
 		}
 
-		this.fragment = new Fragment({
+		this.fragment = fragment = new Fragment({
 			owner: this,
 			template: tpl,
 			cssIds: this.fn._cssIds
 		});
 
-		if ( wasBound ) this.bind();
-		if ( wasRendered ) attach( this, this.fragment );
+		if ( wasBound ) fragment.bind();
+		if ( wasRendered ) attach( this, fragment );
+	},
+
+	refreshAttrs () {
+		keys( this._attrs ).forEach( k => {
+			this._attrs[k].update();
+			this.handle.attrs[k] = this._attrs[k].valueOf();
+		});
 	},
 
 	render ( target, occupants ) {
@@ -91,6 +141,12 @@ assign( proto, {
 
 		this.bound = false;
 		if ( this.fragment ) this.fragment.unbind();
+
+		if ( this._attrs ) {
+			keys( this._attrs ).forEach( k => {
+				this._attrs[k].unbind();
+			});
+		}
 	},
 
 	unrender ( shouldDestroy ) {
@@ -101,8 +157,16 @@ assign( proto, {
 	},
 
 	update () {
-		if ( typeof this.proxy.invalidate === 'function' ) this.proxy.invalidate();
-		if ( this.fragment ) this.fragment.update();
+		if ( this.dirty ) {
+			this.dirty = false;
+
+			if ( this.dirtyAttrs ) {
+				this.refreshAttrs();
+				if ( typeof this.proxy.update === 'function' ) this.proxy.update( this.handle.attrs );
+			}
+			if ( typeof this.proxy.invalidate === 'function' ) this.proxy.invalidate();
+			if ( this.fragment ) this.fragment.update();
+		}
 	}
 });
 
