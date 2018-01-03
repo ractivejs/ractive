14dbdd48582d57e4c887bac486d6e33cd8a837fc
Joseph <fskreuz@gmail.com>
2018-01-02 19:56:31 -0500

    event-keys

---
+++ packages/event-keys/test/index.js
@@ -0,0 +1,34 @@
+import { module, test } from 'qunit'
+import simulant from 'simulant'
+import Ractive from '@ractivejs/core'
+import * as Events from '@ractivejs/event-keys'
+
+module('ractive-event-keys')
+
+const events = {
+  enter: 13,
+  tab: 9,
+  escape: 27,
+  space: 32,
+  leftarrow: 37,
+  rightarrow: 39,
+  downarrow: 40,
+  uparrow: 38
+}
+
+Object.keys(events).forEach(key => {
+  test(key, t => {
+    const instance = Ractive({
+      el: '#qunit-fixture',
+      events: { [key]: Events[key] },
+      template: `<input on-${key}="handler">`,
+      on: {
+        handler () {
+          t.ok(true)
+        }
+      }
+    })
+
+    simulant.fire(instance.find('input'), 'keydown', { which: events[key] })
+  })
+})
