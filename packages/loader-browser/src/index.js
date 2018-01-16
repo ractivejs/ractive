06cbf2e577c2bf2a95450752c9c6cf28261c7c19
Joseph <fskreuz@gmail.com>
2018-01-15 21:55:53 -0500

    Better loader-browser implementation. Reuse requests and constructed components. Shorter code.

--- packages/loader-browser/src/index.js
+++ packages/loader-browser/src/index.js
@@ -1,8 +1,11 @@
 /* eslint-env browser */
 import { toParts, toConstructor, getComponentPath, isComponent } from '@ractivejs/utils-component'
 
-// We need to avoid re-evaluating already-loaded dependencies, hence the cache.
-const resolvedDependencies = {}
+// Two caches are needed:
+// - For pending requests, to reuse pending calls instead of issuing multiple to the same resource.
+// - For the evaluated constructors, to reuse already evaluated components.
+const pending = {}
+const constructors = {}
 
 // Fetch is too new for IE. However, Ractive polyfills Promise, so we're good.
 const get = url => new Promise((resolve, reject) => {
@@ -17,52 +20,30 @@ const get = url => new Promise((resolve, reject) => {
 // - Relative to dependent, if it starts with ./ or ../ (i.e. ./path/to/dependency).
 // - Relative to base, if it starts with a segment (i.e. path/to/dependency).
 // - Relative to domain, if it starts with / (i.e. /path/to/dependency).
-// It's not a question of getComponentPath's implementation, but a question of what
-// is the base for the module to be resolved.
 const resolveModulePath = (m, d, b) => getComponentPath(m, m.charAt(0) === '.' ? d : b)
 
-const load = (basePath, dependentPath, modulePath, dependencyMap) => {
-  // Determine if module is relative to base or dependent
-  const resolvedModulePath = resolveModulePath(modulePath, dependentPath, basePath)
+const load = (basePath, dependentPath, componentPath, dependencyMap) => {
+  const resolvedComponentPath = resolveModulePath(componentPath, dependentPath, basePath)
 
-  // Resolve require calls inside the evaluated code.
   const dependencyResolver = module => {
     const modulePath = resolveModulePath(module, dependentPath, basePath)
-    const resolvedDependency = isComponent(modulePath) ? resolvedDependencies[modulePath] : null
-    const dependency = resolvedDependency || dependencyMap[module] || window[module]
-
-    if (!dependency) throw new Error(`Could not find dependency ${dependency}`)
-
-    return dependency
+    // Resolve from the map and global. Components check the constructor cache first.
+    return (isComponent(modulePath) && constructors[modulePath]) || dependencyMap[module] || window[module]
   }
 
-  return get(resolvedModulePath).then(source => {
-    const parts = toParts(resolvedModulePath, source)
+  return pending[resolvedComponentPath] || (pending[resolvedComponentPath] = get(resolvedComponentPath).then(source => {
+    const parts = toParts(resolvedComponentPath, source)
 
-    // Create and cache the evaluated constructor.
-    const getConstructor = () => {
-      // getConstructor doesn't care what the promise resolves to. If it worked,
-      // the dependencies should be in the resolvedDependencies pile.
-      const constructor = toConstructor(resolvedModulePath, parts, dependencyResolver)
-      resolvedDependencies[resolvedModulePath] = constructor
-      return constructor
-    }
+    // Recursively load ONLY subcomponents. Other deps load either from the map or global.
+    const loadDependencies = dependencies => dependencies.filter(d => isComponent(d.module)).map(d => load(basePath, resolvedComponentPath, d.module, dependencyMap))
 
-    // Recursively load subcomponents
-    const loadDependencies = dependencies => dependencies
-      .filter(d => isComponent(d.module) && !resolvedDependencies[d.module])
-      .map(d => load(basePath, resolvedModulePath, d.module, dependencyMap))
+    // Cache the built constructor for dependencyResolver access.
+    const createConstructor = () => (constructors[resolvedComponentPath] = toConstructor(resolvedComponentPath, parts, dependencyResolver))
 
-    const dependenciesPromise = parts.dependencies ? Promise.all(loadDependencies(parts.dependencies)) : Promise.resolve()
-
-    return dependenciesPromise.then(getConstructor)
-  })
+    // Only built the constructor when all subcomponents resolve.
+    return Promise.all(loadDependencies(parts.dependencies)).then(createConstructor)
+  }))
 }
 
-export const loadComponent = (base, module, dependencyMap) => {
-  return load(base, base, module, dependencyMap)
-}
-
-export const loadComponents = (base, modules, dependencyMap) => {
-  return Promise.all(modules.map(module => load(base, base, module, dependencyMap)))
-}
+export const loadComponent = (base, module, dependencyMap) => load(base, base, module, dependencyMap)
+export const loadComponents = (base, modules, dependencyMap) => Promise.all(modules.map(module => load(base, base, module, dependencyMap)))
--- packages/loader-browser/test/index.js
+++ packages/loader-browser/test/index.js
@@ -3,9 +3,8 @@ import { loadComponent, loadComponents } from '@ractivejs/loader-browser'
 
 module('loader-browser')
 
-test('loadComponent on reference', assert => {
-  return loadComponent('/base/test/samples/reference/', 'component.ractive.html', { answer: 42 }).then(Component => {
-
+test('single', assert => {
+  return loadComponent('/base/test/samples/single/', 'component.ractive.html', { answer: 42 }).then(Component => {
     assert.strictEqual(typeof Component, 'function')
     assert.strictEqual(typeof Component.extend, 'function')
     assert.deepEqual(Component.defaults.data, { message: 'World', answer: 42 })
@@ -31,10 +30,51 @@ test('loadComponent on reference', assert => {
 
     const instance = Component({ el: '#qunit-fixture' })
     const cssId = Component.defaults.cssId
-    const html = `<div data-ractive-css="{${cssId}}">Hello, World!</div> <div data-ractive-css="{${cssId}}">Answer to the Ultimate Question of Life, the Universe, and Everything: 42</div> <div data-ractive-css="{${cssId}}" class="my-component">My Component</div> <div data-ractive-css="{${cssId}}" class="my-other-component">My Other Component</div> <div data-ractive-css="{${cssId}}" class="yet-another-component">Yet Another Component</div>`
+    const html = `<div data-ractive-css='{${cssId}}'>Hello, World!</div> <div data-ractive-css='{${cssId}}'>Answer to the Ultimate Question of Life, the Universe, and Everything: 42</div> <div data-ractive-css='{${cssId}}' class='my-component'>My Component</div> <div data-ractive-css='{${cssId}}' class='my-other-component'>My Other Component</div> <div data-ractive-css='{${cssId}}' class='yet-another-component'>Yet Another Component</div>`
 
     assert.htmlEqual(instance.toHTML(), html)
     assert.htmlEqual(document.getElementById('qunit-fixture').innerHTML, html)
+  })
+})
+
+test('multiple', assert => {
+  return loadComponents('/base/test/samples/multiple/', ['component1.ractive.html', 'component2.ractive.html'], { answer: 42, shared: {} }).then(components => {
+    assert.strictEqual(typeof components[0], 'function')
+    assert.strictEqual(typeof components[0].extend, 'function')
+    assert.deepEqual(components[0].defaults.data, { message: 'World', answer: 42, shared: {} })
+    assert.deepEqual(components[0].defaults.template, { 'v': 4, 't': [{ 't': 7, 'e': 'div', 'm': [{ 'n': 'class', 'f': 'component-1', 't': 13 }], 'f': [{ 't': 7, 'e': 'div', 'f': ['Hello, ', { 't': 2, 'r': 'message' }, '!'] }, ' ', { 't': 7, 'e': 'div', 'f': ['Answer to the Ultimate Question of Life, the Universe, and Everything: ', { 't': 2, 'r': 'answer' }] }, ' ', { 't': 7, 'e': 'MyComponent' }, ' ', { 't': 7, 'e': 'MyOtherComponent' }, ' ', { 't': 7, 'e': 'YetAnotherComponent' }] }] })
+
+    assert.strictEqual(typeof components[1], 'function')
+    assert.strictEqual(typeof components[1].extend, 'function')
+    assert.deepEqual(components[1].defaults.data, { message: 'World', answer: 42, shared: {} })
+    assert.deepEqual(components[1].defaults.template, { 'v': 4, 't': [{ 't': 7, 'e': 'div', 'm': [{ 'n': 'class', 'f': 'component-2', 't': 13 }], 'f': [{ 't': 7, 'e': 'div', 'f': ['Hello, ', { 't': 2, 'r': 'message' }, '!'] }, ' ', { 't': 7, 'e': 'div', 'f': ['Answer to the Ultimate Question of Life, the Universe, and Everything: ', { 't': 2, 'r': 'answer' }] }, ' ', { 't': 7, 'e': 'MyComponent' }, ' ', { 't': 7, 'e': 'MyOtherComponent' }, ' ', { 't': 7, 'e': 'YetAnotherComponent' }] }] })
+
+    components.forEach(Component => {
+      assert.ok(Component.components.hasOwnProperty('MyComponent'))
+      assert.strictEqual(typeof Component.components.MyComponent, 'function')
+      assert.strictEqual(typeof Component.components.MyComponent.extend, 'function')
+      assert.deepEqual(Component.components.MyComponent.defaults.data, {})
+      assert.deepEqual(Component.components.MyComponent.defaults.template, { 'v': 4, 't': [{ 't': 7, 'e': 'div', 'm': [{ 'n': 'class', 'f': 'my-component', 't': 13 }], 'f': ['My Component'] }] })
+
+      assert.ok(Component.components.hasOwnProperty('MyOtherComponent'))
+      assert.strictEqual(typeof Component.components.MyOtherComponent, 'function')
+      assert.strictEqual(typeof Component.components.MyOtherComponent.extend, 'function')
+      assert.deepEqual(Component.components.MyOtherComponent.defaults.data, {})
+      assert.deepEqual(Component.components.MyOtherComponent.defaults.template, { 'v': 4, 't': [{ 't': 7, 'e': 'div', 'm': [{ 'n': 'class', 'f': 'my-other-component', 't': 13 }], 'f': ['My Other Component'] }] })
+
+      assert.ok(Component.components.hasOwnProperty('YetAnotherComponent'))
+      assert.strictEqual(typeof Component.components.YetAnotherComponent, 'function')
+      assert.strictEqual(typeof Component.components.YetAnotherComponent.extend, 'function')
+      assert.deepEqual(Component.components.YetAnotherComponent.defaults.data, {})
+      assert.deepEqual(Component.components.YetAnotherComponent.defaults.template, { 'v': 4, 't': [{ 't': 7, 'e': 'div', 'm': [{ 'n': 'class', 'f': 'yet-another-component', 't': 13 }], 'f': ['Yet Another Component'] }] })
+    })
+
+    // Ensure the dependencies are only ever evaluated once
+    assert.ok(components[0].defaults.data.answer === components[1].defaults.data.answer)
+    assert.ok(components[0].defaults.data.shared === components[1].defaults.data.shared)
+    assert.ok(components[0].components.MyComponent === components[1].components.MyComponent)
+    assert.ok(components[0].components.MyOtherComponent === components[1].components.MyOtherComponent)
+    assert.ok(components[0].components.YetAnotherComponent === components[1].components.YetAnotherComponent)
   })
 })
 
