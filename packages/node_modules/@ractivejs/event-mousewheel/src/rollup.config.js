9ad5e0417fff94350cc2333dd8a35f79c445181f
Joseph <fskreuz@gmail.com>
2018-02-01 13:05:22 -0500

    event-mousewheel

---
+++ packages/node_modules/@ractivejs/event-mousewheel/src/rollup.config.js
@@ -0,0 +1,14 @@
+import pkg from '../package.json'
+import buble from 'rollup-plugin-buble'
+
+export default {
+  sourcemap: true,
+  plugins: [
+    buble({transforms: { modules: false }})
+  ],
+  input: 'src/index.js',
+  output: [
+    { file: pkg.main, format: 'umd', name: pkg.name },
+    { file: pkg.module, format: 'es' }
+  ]
+}
