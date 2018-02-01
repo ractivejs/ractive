3f57267547f1f79ae6c9b42679d45e403db0ec78
Joseph <fskreuz@gmail.com>
2018-02-01 13:06:29 -0500

    loader-system

---
+++ packages/node_modules/@ractivejs/loader-system/test/samples/empty-top-levels/component.js
@@ -0,0 +1,6 @@
+var Ractive = require('@ractivejs/core')
+var component = {exports:{}}
+
+component.exports.template = {"t":[],"v":4}
+component.exports.css = ""
+module.exports = Ractive.extend(component.exports)
