813402c8ca2f26269398c12cfa290ef8a2341a35
Joseph <fskreuz@gmail.com>
2018-02-01 19:43:54 -0500

    Update deps. Replace phantom with chrome.

--- scripts/build-ci-pr.sh
+++ scripts/build-ci-pr.sh
@@ -10,7 +10,7 @@ npm run lint:tests:browser
 npm run lint:tests:node
 
 npm run bundle:test
-npm run test:phantom
+npm run test:chrome
 npm run test:electron
 npm run test:coverage
 
--- scripts/build-local.sh
+++ scripts/build-local.sh
@@ -10,7 +10,7 @@ npm run lint:tests:browser
 npm run lint:tests:node
 
 npm run bundle:test
-npm run test:phantom
+npm run test:chrome
 npm run test:electron || true # Allow to fail for now
 npm run test:coverage
 
