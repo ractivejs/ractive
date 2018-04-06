45b253566f2b8f647b21678fd88ce790800a855b
Chris Reeves <evschris@gmail.com>
2018-04-06 15:17:45 -0400

    properly handle hasListener from a ractive context - refs #3219

--- src/Ractive/prototype/getContext.js
+++ src/Ractive/prototype/getContext.js
@@ -1,7 +1,10 @@
 import staticContext, { getNodeInfo as staticInfo } from '../static/getContext';
+import getRactiveContext from 'src/shared/getRactiveContext';
 import { isString } from 'utils/is';
 
 export default function getContext(node, options) {
+  if (!node) return getRactiveContext(this);
+
   if (isString(node)) {
     node = this.find(node, options);
   }
