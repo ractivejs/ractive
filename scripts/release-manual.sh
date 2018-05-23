d184c623690f52d4caa01898968d0cb180946712
Chris Reeves <evschris@gmail.com>
2018-05-23 14:59:24 -0400

    swizzle manual release process to use the ci script

---
+++ scripts/release-manual.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export TRAVIS_PULL_REQUEST=false
+export FORCE_LOCAL_BUILD=true
+if [ "${FORCE_BRANCH}" != "" ]; then
+  export TRAVIS_BRANCH=$FORCE_BRANCH
+else
+  export TRAVIS_BRANCH=`git branch 2>/dev/null | sed -e '/^[^\*]/d' -e 's/* \(.*\)/\1/'`
+fi
+export GH_REF="git@github.com:ractivejs/ractive"
+
+set -e
+
+echo "This will build your local branch and attempt to publish it. If you want to bail now, you have 5 seconds..."
+sleep 1
+echo "4..."
+sleep 1
+echo "3..."
+sleep 1
+echo "2..."
+sleep 1
+echo "1..."
+sleep 1
+echo "Hold on to your butts...  "
+
+sh scripts/build-ci-dev.sh
+
+sh scripts/release-ci.sh
