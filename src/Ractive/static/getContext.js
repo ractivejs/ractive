1d9c05c034d50461c0c415e87a419b11a7d4f602
Chris Reeves <evschris@gmail.com>
2018-08-07 15:19:29 -0400

    add support for getting a context from non-managed child elements

--- src/Ractive/static/getContext.js
+++ src/Ractive/static/getContext.js
@@ -13,8 +13,8 @@ export default function getContext(node) {
   if (node) {
     if (node._ractive) {
       return node._ractive.proxy.getContext();
-    } else if ((instances = node.__ractive_instances__) && instances.length === 1) {
-      return getRactiveContext(instances[0]);
-    }
+    } else if ((instances = node.__ractive_instances__)) {
+      if (instances.length === 1) return getRactiveContext(instances[0]);
+    } else return getContext(node.parentNode);
   }
 }
