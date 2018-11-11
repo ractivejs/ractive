64576ae790596209586d08053b30fafefe54b418
Travis-CI <evschris+travis@gmail.com>
2018-11-11 18:03:50 +0000

    1.1.0 release

--- ractive.mjs
+++ ractive.mjs
@@ -1,7 +1,7 @@
 /*
-	Ractive.js v1.0.2
-	Build: 27279382f8f568e4c04310a60e3710f7d36abf56
-	Date: Sun Nov 11 2018 18:02:03 GMT+0000 (UTC)
+	Ractive.js v1.1.0
+	Build: 041d7eb7931bc980754b526bb6072adbbfd94ebe
+	Date: Sun Nov 11 2018 18:03:15 GMT+0000 (UTC)
 	Website: https://ractive.js.org
 	License: MIT
 */
@@ -481,13 +481,13 @@ var welcome;
 
 if (hasConsole) {
   var welcomeIntro = [
-    "%cRactive.js %c1.0.2 %cin debug mode, %cmore...",
+    "%cRactive.js %c1.1.0 %cin debug mode, %cmore...",
     'color: rgb(114, 157, 52); font-weight: normal;',
     'color: rgb(85, 85, 85); font-weight: normal;',
     'color: rgb(85, 85, 85); font-weight: normal;',
     'color: rgb(82, 140, 224); font-weight: normal; text-decoration: underline;'
   ];
-  var welcomeMessage = "You're running Ractive 1.0.2 in debug mode - messages will be printed to the console to help you fix problems and optimise your application.\n\nTo disable debug mode, add this line at the start of your app:\n  Ractive.DEBUG = false;\n\nTo disable debug mode when your app is minified, add this snippet:\n  Ractive.DEBUG = /unminified/.test(function(){/*unminified*/});\n\nGet help and support:\n  http://ractive.js.org\n  http://stackoverflow.com/questions/tagged/ractivejs\n  http://groups.google.com/forum/#!forum/ractive-js\n  http://twitter.com/ractivejs\n\nFound a bug? Raise an issue:\n  https://github.com/ractivejs/ractive/issues\n\n";
+  var welcomeMessage = "You're running Ractive 1.1.0 in debug mode - messages will be printed to the console to help you fix problems and optimise your application.\n\nTo disable debug mode, add this line at the start of your app:\n  Ractive.DEBUG = false;\n\nTo disable debug mode when your app is minified, add this snippet:\n  Ractive.DEBUG = /unminified/.test(function(){/*unminified*/});\n\nGet help and support:\n  http://ractive.js.org\n  http://stackoverflow.com/questions/tagged/ractivejs\n  http://groups.google.com/forum/#!forum/ractive-js\n  http://twitter.com/ractivejs\n\nFound a bug? Raise an issue:\n  https://github.com/ractivejs/ractive/issues\n\n";
 
   welcome = function () {
     if (Ractive.WELCOME_MESSAGE === false) {
@@ -7813,6 +7813,26 @@ Context__proto__.add = function add (keypath, d, options) {
 Context__proto__.animate = function animate$1 (keypath, value, options) {
   var model = findModel(this, keypath).model;
   return animate(this.ractive, model, value, options);
+};
+
+Context__proto__.find = function find (selector) {
+  return this.fragment.find(selector);
+};
+
+Context__proto__.findAll = function findAll (selector) {
+  var result = [];
+  this.fragment.findAll(selector, { result: result });
+  return result;
+};
+
+Context__proto__.findAllComponents = function findAllComponents (selector) {
+  var result = [];
+  this.fragment.findAllComponents(selector, { result: result });
+  return result;
+};
+
+Context__proto__.findComponent = function findComponent (selector) {
+  return this.fragment.findComponent(selector);
 };
 
 // get relative keypaths and values
@@ -15261,7 +15281,7 @@ assign(proto$7, {
         this.up = this.component.up;
 
         // {{yield}} is equivalent to {{yield content}}
-        if (!template.r && !template.x && !template.tx) { this.refName = 'content'; }
+        if (!template.r && !template.x && !template.rx) { this.refName = 'content'; }
       } else {
         // plain-ish instance that may be attached to a parent later
         this.fragment = new Fragment({
@@ -16362,6 +16382,13 @@ var Section = (function (MustacheContainer) {
     }
   };
 
+  Section__proto__.bubble = function bubble () {
+    if (!this.dirty && this.yield) {
+      this.dirty = true;
+      this.containerFragment.bubble();
+    } else { MustacheContainer.prototype.bubble.call(this); }
+  };
+
   Section__proto__.detach = function detach () {
     var frag = this.fragment || this.detached;
     return frag ? frag.detach() : MustacheContainer.prototype.detach.call(this);
@@ -16439,6 +16466,17 @@ var Section = (function (MustacheContainer) {
     var siblingFalsey = !this.subordinate || !this.sibling.isTruthy();
     var lastType = this.sectionType;
 
+    if (this.yield && this.yield !== value) {
+      this.up = this.containerFragment;
+      this.container = null;
+      this.yield = null;
+      if (this.rendered) { this.fragment.unbind().unrender(true); }
+      this.fragment = null;
+    } else if (this.rendered && !this.yield && value instanceof Context) {
+      if (this.rendered) { this.fragment.unbind().unrender(true); }
+      this.fragment = null;
+    }
+
     // watch for switching section types
     if (this.sectionType === null || this.templateSectionType === null)
       { this.sectionType = getType(value, this.template.i); }
@@ -16483,6 +16521,15 @@ var Section = (function (MustacheContainer) {
             this.sectionType !== SECTION_IF && this.sectionType !== SECTION_UNLESS
               ? this.model
               : null;
+
+          if (value instanceof Context) {
+            this.yield = value;
+            this.containerFragment = this.up;
+            this.up = value.fragment;
+            this.container = value.ractive;
+            context = undefined;
+          }
+
           newFragment = new Fragment({
             owner: this,
             template: this.template.f
@@ -16525,7 +16572,7 @@ var Section = (function (MustacheContainer) {
 }(MustacheContainer));
 
 function attach(section, fragment) {
-  var anchor = section.up.findNextNode(section);
+  var anchor = (section.containerFragment || section.up).findNextNode(section);
 
   if (anchor) {
     var docFrag = createDocumentFragment();
@@ -19007,7 +19054,7 @@ if (win && !win.Ractive) {
   /* istanbul ignore next */
   if (~opts$1.indexOf('ForceGlobal')) { win.Ractive = Ractive; }
 } else if (win) {
-  warn("Ractive already appears to be loaded while loading 1.0.2.");
+  warn("Ractive already appears to be loaded while loading 1.1.0.");
 }
 
 assign(Ractive.prototype, proto$10, defaults);
@@ -19049,7 +19096,7 @@ defineProperties(Ractive, {
   svg: { value: svg },
 
   // version
-  VERSION: { value: '1.0.2' },
+  VERSION: { value: '1.1.0' },
 
   // plugins
   adaptors: { writable: true, value: {} },
