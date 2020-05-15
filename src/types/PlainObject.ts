c057886cde7a322e9c58b10c9957b6b3c3aa4ebf
Marco Pasqualetti <marco.pasqualetti@live.com>
2020-05-15 08:02:00 +0200

    refactor: convert utils/object to ts

---
+++ src/types/PlainObject.ts
@@ -0,0 +1,3 @@
+export interface PlainObject {
+  [key: string]: never;
+}
---
+++ src/utils/object.ts
@@ -0,0 +1,42 @@
+import { PlainObject } from 'types/PlainObject';
+
+const obj = Object;
+
+// TODO might worth to convert into a class with only static methods?
+
+export const assign = obj.assign;
+
+export const create = obj.create;
+
+export const defineProperty = obj.defineProperty;
+
+export const defineProperties = obj.defineProperties;
+
+export const keys = obj.keys;
+
+export function hasOwn(obj: object, prop: string): boolean {
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}
+
+export function fillGaps(target: PlainObject, ...sources: Array<PlainObject>): PlainObject {
+  for (let i = 0; i < sources.length; i++) {
+    const source = sources[i];
+    for (const key in source) {
+      // Source can be a prototype-less object.
+      if (key in target || !hasOwn(source, key)) continue;
+      target[key] = source[key];
+    }
+  }
+
+  return target;
+}
+
+export function toPairs(obj: PlainObject = {}): Array<PlainObject> {
+  const pairs = [];
+  for (const key in obj) {
+    // Source can be a prototype-less object.
+    if (!hasOwn(obj, key)) continue;
+    pairs.push([key, obj[key]]);
+  }
+  return pairs;
+}
