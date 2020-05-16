70bb35fc33d3d7bb383b2a04d64b0129f06146e5
Marco Pasqualetti <marco.pasqualetti@live.com>
2020-05-16 19:26:09 +0200

    refactor(TransitionManager): restore previuos implementation with functions
    https://github.com/ractivejs/ractive/pull/3334#discussion_r426159545

--- src/global/TransitionManager.ts
+++ src/global/TransitionManager.ts
@@ -3,27 +3,29 @@ import { isFunction } from 'utils/is';
 
 let id = 0;
 
+// TODO add correct types
+
 export default class TransitionManager {
-  private callback: Function;
-  private parent: TransitionManager;
+  public callback: Function;
+  public parent: TransitionManager;
 
-  private intros = [];
-  private outros = [];
-  private children = [];
+  public intros = [];
+  public outros = [];
+  public children = [];
 
-  private totalChildren = 0;
-  private outroChildren = 0;
+  public totalChildren = 0;
+  public outroChildren = 0;
 
   public detachQueue = [];
-  private outrosComplete = false;
+  public outrosComplete = false;
 
-  id: number;
+  public id: number;
 
-  private started: boolean;
+  public started: boolean;
 
-  private notifiedTotal: boolean;
+  public notifiedTotal: boolean;
 
-  constructor(callback: Function, parent: TransitionManager) {
+  constructor(callback, parent) {
     this.callback = callback;
     this.parent = parent;
 
@@ -34,34 +36,34 @@ export default class TransitionManager {
     }
   }
 
-  public add(transition): void {
+  add(transition): void {
     const list = transition.isIntro ? this.intros : this.outros;
     transition.starting = true;
     list.push(transition);
   }
 
-  public addChild(child: TransitionManager): void {
+  addChild(child): void {
     this.children.push(child);
 
     this.totalChildren += 1;
     this.outroChildren += 1;
   }
 
-  public checkStart(): void {
+  checkStart(): void {
     if (this.parent && this.parent.started) this.start();
   }
 
-  private decrementOutros(): void {
+  decrementOutros(): void {
     this.outroChildren -= 1;
-    this.check();
+    check(this);
   }
 
-  private decrementTotal(): void {
+  decrementTotal(): void {
     this.totalChildren -= 1;
-    this.check();
+    check(this);
   }
 
-  private detachNodes(): void {
+  detachNodes(): void {
     let len = this.detachQueue.length;
     for (let i = 0; i < len; i++) this.detachQueue[i].detach();
     len = this.children.length;
@@ -69,111 +71,109 @@ export default class TransitionManager {
     this.detachQueue = [];
   }
 
-  public ready(): void {
-    if (this.detachQueue.length) this.detachImmediate();
+  ready(): void {
+    if (this.detachQueue.length) detachImmediate(this);
   }
 
-  public remove(transition): void {
+  remove(transition): void {
     const list = transition.isIntro ? this.intros : this.outros;
     removeFromArray(list, transition);
-    this.check();
+    check(this);
   }
 
-  public start(): void {
+  start(): void {
     this.started = true;
     this.children.forEach(c => c.start());
     this.intros.concat(this.outros).forEach(t => t.start());
-    this.check();
+    check(this);
   }
+}
 
-  private check(): void {
-    if (!this.started || this.outros.length || this.outroChildren) return;
+function check(tm: TransitionManager): void {
+  if (!tm.started || tm.outros.length || tm.outroChildren) return;
 
-    // If all outros are complete, and we haven't already done this,
-    // we notify the parent if there is one, otherwise
-    // start detaching nodes
-    if (!this.outrosComplete) {
-      this.outrosComplete = true;
+  // If all outros are complete, and we haven't already done this,
+  // we notify the parent if there is one, otherwise
+  // start detaching nodes
+  if (!tm.outrosComplete) {
+    tm.outrosComplete = true;
 
-      if (this.parent) this.parent.decrementOutros();
+    if (tm.parent) tm.parent.decrementOutros();
 
-      if (TransitionManager.allOutrosComplete(this)) {
-        this.detachNodes();
-      }
-    }
-
-    // Once everything is done, we can notify parent transition
-    // manager and call the callback
-    if (!this.intros.length && !this.totalChildren) {
-      if (isFunction(this.callback)) {
-        this.callback();
-      }
-
-      if (this.parent && !this.notifiedTotal) {
-        this.notifiedTotal = true;
-        this.parent.decrementTotal();
-      }
+    if (allOutrosComplete(tm)) {
+      tm.detachNodes();
     }
   }
 
-  // check through the detach queue to see if a node is up or downstream from a
-  // transition and if not, go ahead and detach it
-  private detachImmediate(): void {
-    const queue = this.detachQueue;
-    const outros = TransitionManager.collectAllOutros(this);
+  // Once everything is done, we can notify parent transition
+  // manager and call the callback
+  if (!tm.intros.length && !tm.totalChildren) {
+    if (isFunction(tm.callback)) {
+      tm.callback();
+    }
 
-    if (!outros.length) {
-      this.detachNodes();
-    } else {
-      let i = queue.length;
-      let j = 0;
-      let node, trans;
-      const nqueue = (this.detachQueue = []);
+    if (tm.parent && !tm.notifiedTotal) {
+      tm.notifiedTotal = true;
+      tm.parent.decrementTotal();
+    }
+  }
+}
 
-      start: while (i--) {
-        node = queue[i].node;
-        j = outros.length;
-        while (j--) {
-          trans = outros[j].element.node;
-          // check to see if the node is, contains, or is contained by the transitioning node
-          if (trans === node || trans.contains(node) || node.contains(trans)) {
-            nqueue.push(queue[i]);
-            continue start;
-          }
+function allOutrosComplete(manager: TransitionManager): any {
+  return !manager || (manager.outrosComplete && allOutrosComplete(manager.parent));
+}
+
+// check through the detach queue to see if a node is up or downstream from a
+// transition and if not, go ahead and detach it
+function detachImmediate(manager: TransitionManager): void {
+  const queue = manager.detachQueue;
+  const outros = collectAllOutros(manager);
+
+  if (!outros.length) {
+    manager.detachNodes();
+  } else {
+    let i = queue.length;
+    let j = 0;
+    let node, trans;
+    const nqueue = (manager.detachQueue = []);
+
+    start: while (i--) {
+      node = queue[i].node;
+      j = outros.length;
+      while (j--) {
+        trans = outros[j].element.node;
+        // check to see if the node is, contains, or is contained by the transitioning node
+        if (trans === node || trans.contains(node) || node.contains(trans)) {
+          nqueue.push(queue[i]);
+          continue start;
         }
-
-        // no match, we can drop it
-        queue[i].detach();
-      }
-    }
-  }
-
-  private static allOutrosComplete(manager: TransitionManager): boolean {
-    return (
-      !manager || (manager.outrosComplete && TransitionManager.allOutrosComplete(manager.parent))
-    );
-  }
-
-  private static collectAllOutros(manager: TransitionManager, _list?): any[] {
-    let list = _list;
-
-    // if there's no list, we're starting at the root to build one
-    if (!list) {
-      list = [];
-      let parent = manager;
-      while (parent.parent) parent = parent.parent;
-      return TransitionManager.collectAllOutros(parent, list);
-    } else {
-      // grab all outros from child managers
-      let i = manager.children.length;
-      while (i--) {
-        list = TransitionManager.collectAllOutros(manager.children[i], list);
       }
 
-      // grab any from this manager if there are any
-      if (manager.outros.length) list = list.concat(manager.outros);
-
-      return list;
+      // no match, we can drop it
+      queue[i].detach();
     }
+  }
+}
+
+function collectAllOutros(manager: TransitionManager, _list?): any[] {
+  let list = _list;
+
+  // if there's no list, we're starting at the root to build one
+  if (!list) {
+    list = [];
+    let parent = manager;
+    while (parent.parent) parent = parent.parent;
+    return collectAllOutros(parent, list);
+  } else {
+    // grab all outros from child managers
+    let i = manager.children.length;
+    while (i--) {
+      list = collectAllOutros(manager.children[i], list);
+    }
+
+    // grab any from this manager if there are any
+    if (manager.outros.length) list = list.concat(manager.outros);
+
+    return list;
   }
 }
