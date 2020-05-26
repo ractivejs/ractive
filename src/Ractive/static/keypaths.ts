6b3b7517d875a395f8fc54c53ef7fe2664f22795
Marco Pasqualetti <marco.pasqualetti@live.com>
2020-05-26 16:50:17 +0200

    refactor(Ractive/static/keypaths): convert to ts

--- src/Ractive/static/keypaths.ts
+++ src/Ractive/static/keypaths.ts
@@ -1,9 +1,10 @@
 import { escapeKey, splitKeypath as splitKeypathI, unescapeKey } from 'shared/keypaths';
+import { Keypath } from 'types/Keypath';
 
-export function joinKeys(...keys) {
+export function joinKeys(...keys: string[]): Keypath {
   return keys.map(escapeKey).join('.');
 }
 
-export function splitKeypath(keypath) {
+export function splitKeypath(keypath: Keypath): string[] {
   return splitKeypathI(keypath).map(unescapeKey);
 }
