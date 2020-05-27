13a37e7db50017be7d0c2e4a4fd9f2f6db5946de
Marco Pasqualetti <marco.pasqualetti@live.com>
2020-05-27 19:48:31 +0200

    refactor(parse/converters/expressions/readKey): convert to ts

--- src/parse/converters/expressions/shared/readKey.ts
+++ src/parse/converters/expressions/shared/readKey.ts
@@ -1,13 +1,19 @@
 import readStringLiteral from '../primary/literal/readStringLiteral';
 import readNumberLiteral from '../primary/literal/readNumberLiteral';
 import { name as namePattern } from './patterns';
+import { LiteralTemplateElement } from 'parse/templateElements';
 
 const identifier = /^[a-zA-Z_$][a-zA-Z_$0-9]*$/;
 
-// http://mathiasbynens.be/notes/javascript-properties
-// can be any name, string literal, or number literal
-export default function readKey(parser) {
-  let token;
+/**
+ * can be any name, string literal, or number literal
+ *
+ * @see http://mathiasbynens.be/notes/javascript-properties
+ *
+ * @todo add parser type
+ */
+export default function readKey(parser): string {
+  let token: LiteralTemplateElement | string;
 
   if ((token = readStringLiteral(parser))) {
     return identifier.test(token.v) ? token.v : '"' + token.v.replace(/"/g, '\\"') + '"';
@@ -17,7 +23,7 @@ export default function readKey(parser) {
     return token.v;
   }
 
-  if ((token = parser.matchPattern(namePattern))) {
+  if ((token = parser.matchPattern(namePattern) as string)) {
     return token;
   }
 
