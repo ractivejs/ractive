6aed392c346fad32dc4f136736de02f0b68e2231
Marco Pasqualetti <marco.pasqualetti@live.com>
2020-05-29 11:28:44 +0200

    refactor(parse/getLowestIndex): convert to ts

--- src/parse/converters/utils/getLowestIndex.ts
+++ src/parse/converters/utils/getLowestIndex.ts
@@ -2,7 +2,7 @@ import escapeRegExp from 'utils/escapeRegExp';
 
 const regExpCache = {};
 
-export default function(haystack, needles) {
+export default function(haystack: string, needles: string[]): number {
   return haystack.search(
     regExpCache[needles.join()] ||
       (regExpCache[needles.join()] = new RegExp(needles.map(escapeRegExp).join('|')))
