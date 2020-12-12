b2579d6c19f4795f941ed997d97c3c7472684bfb
Travis-CI <evschris+travis@gmail.com>
2020-12-12 08:01:38 +0000

    1.3.14 release

--- typings/ractive.d.ts
+++ typings/ractive.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for Ractive edge
 // Project: https://ractive.js.org/
 // Definitions By: Chris Reeves <https://github.com/evs-chris>
-// Version: 1.0.0-edge+2018-04-22
+// Version: 1.3
 
 export interface ValueMap {
 	[key: string]: any;
@@ -541,6 +541,7 @@ export class MacroHelper extends ContextHelper {
 }
 
 export interface MacroOpts {
+	attributes?: string[];
 	cssId?: string;
 	noCssTransform?: boolean;
 	css?: string | CssFn;
@@ -1602,7 +1603,7 @@ export module Ractive {
 	 * @param macro
 	 * @param options
 	 */
-	function macro(macro: MacroFn, options: MacroOpts): Macro;
+	function macro(macro: MacroFn, options?: MacroOpts): Macro;
 
 	/**
 	 * Parse the given template string into a template.j
